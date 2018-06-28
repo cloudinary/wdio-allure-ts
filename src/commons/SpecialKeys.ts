a31fd4e908710988bc80b2baabd9e2ccd6d2bafb
Felix Zilber <felix@cloudinary.com>
2018-06-28 16:31:44 +0300

    add tab to specialKeys

--- src/commons/SpecialKeys.ts
+++ src/commons/SpecialKeys.ts
@@ -3,5 +3,6 @@
  * will be added more by demand according to https://w3c.github.io/webdriver/webdriver-spec.html#keyboard-actions
  */
 export enum SpecialKeys {
-  Enter = "\uE007"
+  Enter = "\uE007",
+  Tab = "\uE004"
 }
