dc14893972886f2defc208b7ffb11bb09beb909c
Ad1B3nAr0ya <adi@cloudinary.com>
2018-10-28 16:16:34 +0200

    fixed conflicts

--- test/specs/GetAttributeSpec.ts
+++ test/specs/GetAttributeSpec.ts
@@ -1,31 +1,31 @@
-import { assert, expect } from "chai";
-import { BrowserUtils } from "../../src/commons/BrowserUtils";
+import { assert, expect } from 'chai';
+import { BrowserUtils } from '../../src/commons/BrowserUtils';
 
 // tslint:disable-next-line:no-http-string
-const sampleAppUrl: string = "http://127.0.0.1:8000/";
+const sampleAppUrl: string = 'http://127.0.0.1:8000/';
 
 /**
  * wdio-allure-ts getAttribute action test
  */
-describe("GetAttributeSpec of BrowserUtils Tests", () => {
-    it("Validate positive result ", () => {
-        BrowserUtils.navigateToUrl(sampleAppUrl);
-        assert.equal(BrowserUtils.getAttribute("//form",'method'), "post");
-    });
+describe('GetAttributeSpec of BrowserUtils Tests', () => {
+  it('Validate positive result ', () => {
+    BrowserUtils.navigateToUrl(sampleAppUrl);
+    assert.equal(BrowserUtils.getAttribute('//form', 'method'), 'post');
+  });
 
-    it("Validate null result ending in err", () => {
-        BrowserUtils.navigateToUrl(sampleAppUrl);
-        expect(() => BrowserUtils.getAttribute("//form", 'name'))
-            .to.throw(Error)
-            .with.property("message")
-            .contains(`Failed to get name attribute`);
-    });
+  it('Validate null result ending in err', () => {
+    BrowserUtils.navigateToUrl(sampleAppUrl);
+    expect(() => BrowserUtils.getAttribute('//form', 'name'))
+      .to.throw(Error)
+      .with.property('message')
+      .contains(`Failed to get name attribute`);
+  });
 
-    it("Validate incorrect value ending in err", () => {
-        BrowserUtils.navigateToUrl(sampleAppUrl);
-        expect(() => BrowserUtils.getAttribute("//form", 'ONG'))
-            .to.throw(Error)
-            .with.property("message")
-            .contains(`Failed to get ONG`);
-    });
+  it('Validate incorrect value ending in err', () => {
+    BrowserUtils.navigateToUrl(sampleAppUrl);
+    expect(() => BrowserUtils.getAttribute('//form', 'ONG'))
+      .to.throw(Error)
+      .with.property('message')
+      .contains(`Failed to get ONG`);
+  });
 });
