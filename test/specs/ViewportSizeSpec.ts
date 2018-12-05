68b12b2c3cb85d45fa2109c963647a4751c65b89
Ad1B3nAr0ya <adi@cloudinary.com>
2018-12-05 14:42:44 +0200

    added CR requests

--- test/specs/ViewportSizeSpec.ts
+++ test/specs/ViewportSizeSpec.ts
@@ -4,6 +4,8 @@ import Size = WebdriverIO.Size;
 
 // tslint:disable-next-line:no-http-string
 const sampleAppUrl: string = 'http://127.0.0.1:8000/';
+const DEFAULT_WIDTH: number = 1024;
+const DEFAULT_HEIGHT: number = 768;
 
 /**
  * wdio-allure-ts Change window size and get window size (setViewportSize, getViewportSize)
@@ -12,19 +14,18 @@ describe('ViewportSizeSpec of BrowserUtils Tests', () => {
 
     it('Validate window size after size change to 1024x768', () => {
         BrowserUtils.navigateToUrl(sampleAppUrl);
-        //BrowserUtils.setWindowSize({width:1024, height:768});
-        BrowserUtils.setViewportSize({ width: 1024, height: 768 });
+        BrowserUtils.setViewportSize({ width: DEFAULT_WIDTH, height: DEFAULT_HEIGHT });
         browser.pause(1000);
-        assert.equal(browser.execute(()=> window.innerWidth).value, 1024, 'window width');
-        assert.equal(browser.execute(()=> window.innerHeight).value, 768, 'window height');
+        assert.equal(browser.execute(()=> window.innerWidth).value, DEFAULT_WIDTH, 'window width');
+        assert.equal(browser.execute(()=> window.innerHeight).value, DEFAULT_HEIGHT, 'window height');
     });
 
     it('Validate window size after size change to width = 1024', () => {
         BrowserUtils.navigateToUrl(sampleAppUrl);
-        BrowserUtils.setViewportSize({width: 1024, height: undefined});
+        BrowserUtils.setViewportSize({width: DEFAULT_WIDTH, height: undefined});
         const size : Size = BrowserUtils.getViewportSize();
-        assert.equal(size.width, 1024, 'window width');
-        assert.equal(size.height, 768, 'window height');
+        assert.equal(size.width, DEFAULT_WIDTH, 'window width');
+        assert.equal(size.height, DEFAULT_HEIGHT, 'window height');
     });
 
 
@@ -32,15 +33,15 @@ describe('ViewportSizeSpec of BrowserUtils Tests', () => {
         BrowserUtils.navigateToUrl(sampleAppUrl);
         BrowserUtils.setViewportSize({width: undefined, height: 768});
         const size : Size = BrowserUtils.getViewportSize();
-        assert.equal(size.width, 1024, 'window width');
-        assert.equal(size.height, 768, 'window height');
+        assert.equal(size.width, DEFAULT_WIDTH, 'window width');
+        assert.equal(size.height, DEFAULT_HEIGHT, 'window height');
     });
 
     it('Validate no change requested (action aborted)', () => {
         BrowserUtils.navigateToUrl(sampleAppUrl);
         BrowserUtils.setViewportSize({width: undefined, height: undefined});
         const size : Size = BrowserUtils.getViewportSize();
-        assert.equal(size.width, 1024, 'window width');
-        assert.equal(size.height, 768, 'window height');
+        assert.equal(size.width, DEFAULT_WIDTH, 'window width');
+        assert.equal(size.height, DEFAULT_HEIGHT, 'window height');
     });
 });
