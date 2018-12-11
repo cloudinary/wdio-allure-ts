3292a58e25b1f77e00a6b122f8f08341ceaa40b1
Felix Zilber <felix@cloudinary.com>
2018-12-11 22:28:21 +0200

    wdio conf update

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -1,75 +1,62 @@
-var fs = require("fs");
-var dotenv = require("dotenv");
+//Set short timeout for test
+process.env.DEFAULT_TIME_OUT = '10000';
 
 exports.config = {
+  //
   // =====================
   // Browser Configurations
   // =====================
   capabilities: [
     {
       maxInstances: 1,
-      browserName: "chrome",
+      browserName: 'chrome',
 
       chromeOptions: {
-        args: ["--headless", "--incognito"]
+        args: ['--headless', '--incognito'],
       },
-     },
+    },
     {
       maxInstances: 1,
-      browserName: "firefox",
-      "moz:firefoxOptions": {
-        args: ["--headless"]
-      }
-    }
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: ['--headless'],
+      },
+    },
   ],
 
   suites: {
-    regression: ["./lib/test/specs/*Spec.js"]
+    regression: ['./lib/test/specs/*Spec.js'],
   },
 
   // ===================
   // Test Configurations
   // ===================
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 6000,
+  waitforTimeout: 10000,
   //
   // Default timeout in milliseconds for request
   // if Selenium Grid doesn't send response
-  connectionRetryTimeout: 6000,
+  connectionRetryTimeout: 10000,
   //
   // Default request retries count
   connectionRetryCount: 3,
 
-  framework: "mocha",
+  framework: 'mocha',
   //
   // Test reporter for stdout.
   // see also: http://webdriver.io/guide/reporters/dot.html
-  reporters: ["spec"],
+  reporters: ['spec'],
 
   /**
    * webdriver service
    */
-  services: ["selenium-standalone"],
+  services: ['selenium-standalone'],
 
   //
   // Options to be passed to Mocha.
   // See the full list at http://mochajs.org/
   mochaOpts: {
-    ui: "bdd",
-    timeout: 300000 // test timeout, test will fall after 5 minutes on timeout
+    ui: 'bdd',
+    timeout: 50000, // test timeout, test will fall after 5 minutes on timeout
   },
-  // =====
-  // Hooks
-  // =====
-  /**
-   * Gets executed once before all workers get launched.
-   * @param {Object} config wdio configuration object
-   * @param {Array.<Object>} capabilities list of capabilities details
-   */
-  onPrepare: function(config, capabilities) {
-    /**
-     * Load configurations from .env
-     */
-    dotenv.config();
-  }
 };
