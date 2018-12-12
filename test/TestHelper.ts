df6c2d2ebf0817b3e719bcd5c0c56d62b4918f5b
Felix Zilber <felix@cloudinary.com>
2018-12-12 23:58:30 +0200

    remove page refresh before each spec

--- test/TestHelper.ts
+++ test/TestHelper.ts
@@ -14,14 +14,11 @@ export const sampleAppUrl: string = 'http://127.0.0.1:8000/';
 // tslint:disable-next-line:export-name
 export function describeCommon(name: string, body: Function): void {
   describe(`${name}`, () => {
+    /**
+     * Navigate to sampleApp and wait for it to load
+     */
     before(() => {
       BrowserUtils.navigateToUrl(sampleAppUrl);
-    });
-    /**
-     * Navigate to sampleApp and refresh the page
-     */
-    beforeEach(() => {
-      browser.refresh();
       browser.waitForVisible("//*[@data-test='print-message-btn']");
     });
 
