9a75caef8f37e9bfcfaac5a9f812e0fa3f82606c
Felix Zilber <felix@cloudinary.com>
2019-01-28 16:09:08 +0200

    change setViewportSize to setWindowSize and update tests

---
+++ test/specs/WindowSizeSpec.ts
@@ -0,0 +1,18 @@
+import { assert } from 'chai';
+import { BrowserUtils } from '../../src/commons/BrowserUtils';
+
+let size: { width?: number; height?: number } = {};
+/**
+ * wdio-allure-ts Change window size and get window size
+ */
+describe('setWindowSize', () => {
+  it('change window size', () => {
+    const width: number = 800;
+    const height: number = 600;
+    BrowserUtils.setWindowSize(width, height);
+
+    size = browser.getWindowSize();
+    assert.equal(size.width, width, 'window width');
+    assert.equal(size.height, height, 'window height');
+  });
+});
