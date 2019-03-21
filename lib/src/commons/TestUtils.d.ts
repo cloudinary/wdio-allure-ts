e4821f5944dd0d88b5b9c0549ba5bde642d0a5bd
Ad1B3nAr0ya <adi@cloudinary.com>
2019-03-21 11:01:44 +0200

    lib

---
+++ lib/src/commons/TestUtils.d.ts
@@ -0,0 +1,10 @@
+/**
+ * General utils useful for test
+ */
+export declare namespace TestUtils {
+    /**
+     * Returns random string of requested length
+     * @param length length of returned string
+     */
+    function randomString(length: number): string;
+}
