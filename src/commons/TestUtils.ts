f8fe1d6e558143b41a9ddf78f43f3e6ed482d7dd
Ad1B3nAr0ya <adi@cloudinary.com>
2019-03-26 13:42:34 +0200

    added special chars escaping method

--- src/commons/TestUtils.ts
+++ src/commons/TestUtils.ts
@@ -22,4 +22,42 @@ export namespace TestUtils {
 
     return text;
   }
+
+  /**
+   * Elastic search query prohibits special chars without escaping
+   * More info: https://discuss.elastic.co/t/how-to-index-special-characters-and-search-those-special-characters-in-elasticsearch/42506
+   * Escaping special characters that are part of the query syntax. The current list special characters are: + - && || ! ( ) { } [ ] ^ " ~ * ? : \
+   * To escape these character use the \ before the character
+   * @param inputString - string that might contain special characters that needs escaping
+   */
+  export function escapeMetaCharacters(inputString: string): string {
+    const metaCharacters: string[] = [
+      '\\',
+      '^',
+      '$',
+      '{',
+      '}',
+      '[',
+      ']',
+      '(',
+      ')',
+      '*',
+      '+',
+      '?',
+      '|',
+      '<',
+      '>',
+      '-',
+      '&',
+      '%',
+    ];
+
+    for (const char of metaCharacters) {
+      if (inputString.includes(char)) {
+        inputString.replace(char, `\/${char}`);
+      }
+    }
+
+    return inputString;
+  }
 }
