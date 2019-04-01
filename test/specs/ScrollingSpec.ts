e738d3d4746c5d243d4ac94346bbb0a8ae0e9eee
Ad1B3nAr0ya <adi@cloudinary.com>
2019-04-01 17:05:04 +0300

    added scrollingSpec section to html and adjusted tests

--- test/specs/ScrollingSpec.ts
+++ test/specs/ScrollingSpec.ts
@@ -1,12 +1,12 @@
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { BrowserUtils } from '../../src/commons/BrowserUtils';
 import { describeCommon } from '../TestHelper';
 
 export namespace PageLocator {
   export const HEADER: string = "//*[@id='top']/header/h1";
-  export const CAT_IMAGE: string = "//*[@id='embedded__images']/div/p/img";
-  export const SELECTION_LIST: string = "//*[@id='select']";
-  export const LIST_ITEM_THREE: string = "//*[@id='select']/optgroup/option[3]";
+  export const LAST_TOP_LINK: string = "//*[@id='lastTop']";
+  export const SELECTION_LIST: string = "//*[@id='selectList']";
+  export const LIST_ITEM_THREE: string = `${SELECTION_LIST}/optgroup/option[3]`;
 }
 
 /**
@@ -15,15 +15,17 @@ export namespace PageLocator {
 describeCommon('Scroll', () => {
   it('scrollIntoView ', () => {
     BrowserUtils.waitForDisplayed(PageLocator.HEADER);
-    assert.isFalse($(PageLocator.CAT_IMAGE).isDisplayedInViewport());
-    BrowserUtils.scrollIntoView(PageLocator.CAT_IMAGE);
-    assert.isTrue($(PageLocator.CAT_IMAGE).isDisplayedInViewport());
+    assert.isFalse($(PageLocator.LAST_TOP_LINK).isDisplayedInViewport());
+    BrowserUtils.scrollIntoView(PageLocator.LAST_TOP_LINK);
+    assert.isTrue($(PageLocator.LAST_TOP_LINK).isDisplayedInViewport());
   });
 
-  it.skip('scrollToElement', () => {
-    BrowserUtils.scrollToElement(
-      PageLocator.LIST_ITEM_THREE,
-      PageLocator.SELECTION_LIST
-    );
+  it('scrollToElement', () => {
+    expect(() =>
+      BrowserUtils.scrollToElement(
+        PageLocator.LIST_ITEM_THREE,
+        PageLocator.SELECTION_LIST
+      )
+    ).not.to.throw(Error);
   });
 });
