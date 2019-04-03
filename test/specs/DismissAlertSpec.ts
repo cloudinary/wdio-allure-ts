766b0224ba10267c2b91aebc6cabeba60330b7c9
Felix Zilber <felix@cloudinary.com>
2019-04-04 01:16:04 +0300

    remove redundant beforeEach from test

--- test/specs/DismissAlertSpec.ts
+++ test/specs/DismissAlertSpec.ts
@@ -6,10 +6,6 @@ const TEST_FIELD_SELECTOR: string = "//*[@id='DismissAlert']";
 const TRIGGER_ALERT_BUTTON_SELECTOR: string = `${TEST_FIELD_SELECTOR}//button[@id='trigger-alert']`;
 
 describeCommon('dismissAlert', () => {
-  beforeEach(() => {
-    browser.refresh();
-  });
-
   it('dismiss existing alert', () => {
     $(TRIGGER_ALERT_BUTTON_SELECTOR).click();
     BrowserUtils.dismissAlert();
