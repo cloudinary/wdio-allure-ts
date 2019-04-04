381a2f1e83d927d0adffdf7540e94cc8ac7a4e89
Ad1B3nAr0ya <adi@cloudinary.com>
2019-04-04 10:16:08 +0300

    fixed header locator

--- test/specs/ScrollingSpec.ts
+++ test/specs/ScrollingSpec.ts
@@ -3,10 +3,8 @@ import { BrowserUtils } from '../../src/commons/BrowserUtils';
 import { describeCommon } from '../TestHelper';
 
 export namespace PageLocator {
-  export const HEADER: string = "//*[@id='top']/header/h1";
+  export const HEADER: string = "//*[@id='header']";
   export const CAT_IMAGE: string = "//*[@id='embedded__images']/div/p/img";
-  export const SELECTION_LIST: string = "//*[@id='select']";
-  export const LIST_ITEM_THREE: string = "//*[@id='select']/optgroup/option[3]";
 }
 
 /**
@@ -20,10 +18,4 @@ describeCommon('Scroll', () => {
     assert.isTrue($(PageLocator.CAT_IMAGE).isDisplayedInViewport());
   });
 
-  it.skip('scrollToElement', () => {
-    BrowserUtils.scrollToElement(
-      PageLocator.LIST_ITEM_THREE,
-      PageLocator.SELECTION_LIST
-    );
-  });
 });
