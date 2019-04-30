05671b62b4a2cbf9bcaf1dae5796712a67e2f2d5
alexeykagansky <alexey.kagansky@cloudinary.com>
2019-04-30 11:44:24 +0300

    add new expectCurrentUrlSpec

---
+++ test/specs/ExpectCurrentUrlSpec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { BrowserUtils } from '../../src/commons/BrowserUtils';
+import { describeCommon } from '../TestHelper';
+
+const NAVIGATION_BUTTON_SELECTOR: string = '//button[@id="openCloudinaryUrl"]';
+const CLOUDINARY_URL: string = 'https://cloudinary.com';
+/**
+ * wdio-allure-ts ExpectCurrentUrlSpec
+ */
+describeCommon('expectCurrentUrl', () => {
+  it('correct url', () => {
+    $(NAVIGATION_BUTTON_SELECTOR).click();
+    expect(() => BrowserUtils.expectCurrentUrl(CLOUDINARY_URL)).to.not.throw(
+      Error
+    );
+  });
+
+  it('incorrect url', () => {
+    expect(() => BrowserUtils.expectCurrentUrl(CLOUDINARY_URL))
+      .to.throw(Error)
+      .with.property('message')
+      .equal(`Url not as expected '${CLOUDINARY_URL}'`);
+  });
+});
