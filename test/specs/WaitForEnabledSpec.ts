3cc7eebb9b2214e517f6a34d8e4fb02e17103c07
alexeykagansky <alexey.kagansky@cloudinary.com>
2019-04-30 15:25:16 +0300

    add new waitForEnabledSpec

--- test/specs/WaitForEnabledSpec.ts
+++ test/specs/WaitForEnabledSpec.ts
@@ -1,25 +1,29 @@
-import waitForEnabled = BrowserUtils.waitForEnabled;
 import { expect } from 'chai';
 import { BrowserUtils } from '../../src/commons/BrowserUtils';
 import { describeCommon } from '../TestHelper';
 
-const ENABLED_ELEMENT_SELECTOR: string = "//input[@id='enabled_input']";
-const DISABLED_ELEMENT_SELECTOR: string = "//input[@id='disabled_input']";
-const NOT_EXISTS_ELEMENT_SELECTOR: string = "//input[@id='non_exists_input']";
+const ENABLED_ELEMENT_SELECTOR: string =
+  "//button[@id='click_to_enable_second_button']";
+const DISABLED_ELEMENT_SELECTOR: string = "//button[@id='disabled_button']";
+const NOT_EXISTS_ELEMENT_SELECTOR: string =
+  "//button[@id='disabled_button1234']";
 describeCommon('waitForEnabled', () => {
   it('element enabled', () => {
-    expect(() => waitForEnabled(ENABLED_ELEMENT_SELECTOR)).to.not.throw(Error);
+    BrowserUtils.click(ENABLED_ELEMENT_SELECTOR);
+    expect(() =>
+      BrowserUtils.waitForEnabled(ENABLED_ELEMENT_SELECTOR)
+    ).to.not.throw(Error);
   });
   it('element disabled', () => {
-    expect(() => waitForEnabled(ENABLED_ELEMENT_SELECTOR))
-      .to.not.throw(Error)
+    expect(() => BrowserUtils.waitForEnabled(DISABLED_ELEMENT_SELECTOR))
+      .to.throw(Error)
       .with.property('message')
-      .equal(`The selector  ${DISABLED_ELEMENT_SELECTOR} is not enabled `);
+      .contains(`Element not enabled`);
   });
   it('element not exists', () => {
-    expect(() => waitForEnabled(ENABLED_ELEMENT_SELECTOR))
-      .to.not.throw(Error)
+    expect(() => BrowserUtils.waitForEnabled(NOT_EXISTS_ELEMENT_SELECTOR))
+      .to.throw(Error)
       .with.property('message')
-      .equal(`The selector  ${NOT_EXISTS_ELEMENT_SELECTOR} is not enabled `);
+      .contains(`Element not exist`);
   });
 });
