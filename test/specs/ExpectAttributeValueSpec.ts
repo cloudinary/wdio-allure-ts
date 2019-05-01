837418e8e5cc6a8c9896b2aa59acb2ccaea5f953
alexeykagansky <alexey.kagansky@cloudinary.com>
2019-05-01 12:40:57 +0300

    add new expectAttributeValueSpec

--- test/specs/ExpectAttributeValueSpec.ts
+++ test/specs/ExpectAttributeValueSpec.ts
@@ -4,7 +4,10 @@ import { describeCommon } from '../TestHelper';
 
 const SELECTOR_WITH_ATTRIBUTE: string =
   '//button[@id="button_with_attribute_value"]';
+const INCORRECT_SELECTOR: string =
+  '//button[@id="button_with_attribute_value`-incorrect"]';
 const ATTRIBUTE_NAME: string = 'value';
+const INCORRECT_ATTRIBUTE_NAME: string = 'text';
 const CORRECT_ATTRIBUTE_VALUE: string = 'hello world';
 const INCORRECT_ATTRIBUTE_VALUE: string = 'hello hello world';
 
@@ -27,6 +30,30 @@ describeCommon('expectAttributeValue', () => {
       BrowserUtils.expectAttributeValue(
         SELECTOR_WITH_ATTRIBUTE,
         ATTRIBUTE_NAME,
+        INCORRECT_ATTRIBUTE_VALUE
+      )
+    )
+      .to.throw(Error)
+      .with.property('message')
+      .contains(`Incorrect attribute`);
+  });
+  it('incorrect selector', () => {
+    expect(() =>
+      BrowserUtils.expectAttributeValue(
+        INCORRECT_SELECTOR,
+        ATTRIBUTE_NAME,
+        INCORRECT_ATTRIBUTE_VALUE
+      )
+    )
+      .to.throw(Error)
+      .with.property('message')
+      .contains(`Incorrect attribute`);
+  });
+  it('incorrect attribute', () => {
+    expect(() =>
+      BrowserUtils.expectAttributeValue(
+        INCORRECT_SELECTOR,
+        INCORRECT_ATTRIBUTE_NAME,
         INCORRECT_ATTRIBUTE_VALUE
       )
     )
