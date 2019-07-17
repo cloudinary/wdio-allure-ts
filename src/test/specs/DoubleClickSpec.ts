6213d9dc595be906376033cfba6cefce4b783ab8
Felix Zilber <felix@cloudinary.com>
2019-07-17 21:53:21 +0300

    add doubleClickSpec

---
+++ src/test/specs/DoubleClickSpec.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import { BrowserUtils } from '../..';
+import { describeCommon } from '../TestHelper';
+
+/**
+ * doubleClick
+ */
+describeCommon('doubleClick', () => {
+  it('successful double click', () => {
+    expect(() => BrowserUtils.doubleClick("//button[@id='doubleClickToEnable']")).to.not.throw(Error);
+    expect(() => BrowserUtils.waitForEnabled("//button[@id='doubleClickWillBeEnabledButton']")).to.not.throw(Error);
+  });
+
+  it('double click not existing element', () => {
+    const notExistingElementSelector: string = '//notARealSelector';
+    expect(() => BrowserUtils.doubleClick(notExistingElementSelector))
+      .to.throw(Error)
+      .with.property('message')
+      .contains(` element ("${notExistingElementSelector}") still not existing`);
+  });
+
+  it('double click hidden element', () => {
+    const hiddenElementSelector: string = "//button[@id='doubleClickHidden']";
+    expect(() => BrowserUtils.doubleClick(hiddenElementSelector))
+      .to.throw(Error)
+      .with.property('message')
+      .contains(`Element not visible '${hiddenElementSelector}'`);
+  });
+
+  it('double click disabled element', () => {
+    const disableElementSelector: string = "//button[@id='doubleClickDisabledButton']";
+    expect(() => BrowserUtils.doubleClick(disableElementSelector))
+      .to.throw(Error)
+      .with.property('message')
+      .contains(`Element not enabled '${disableElementSelector}'`);
+  });
+});
