7487272bcdf1e17594566bcef955b58918fb4035
Ad1B3nAr0ya <adi@cloudinary.com>
2019-07-17 11:44:38 +0300

    added tests for expectNoAttributeValue

--- src/test/specs/ExpectNoAttributeValueSpec.ts
+++ src/test/specs/ExpectNoAttributeValueSpec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { BrowserUtils } from '../../commons/BrowserUtils';
-import { describeCommon } from '../TestHelper';
+import { describeCommon, sampleAppUrl } from '../TestHelper';
 
 const EMPTY_DIV: string = '//*[@id="formsWithoutAttribute"]//*[@id="noAttDiv"]';
 /**
@@ -17,5 +17,15 @@ describeCommon('expectNoAttributeValue', () => {
 
   it('Exact match error thrown', () => {
     expect(() => BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'align', 'center')).to.throw(Error);
+  });
+
+  it('Case Element not found.', () => {
+    BrowserUtils.navigateToUrl(sampleAppUrl);
+    expect(() => BrowserUtils.expectNoAttributeValue('//NotExist', 'method', 'post')).to.throw(Error);
+  });
+
+  it('Case attribute not found', () => {
+    BrowserUtils.navigateToUrl(sampleAppUrl);
+    expect(() => BrowserUtils.expectNoAttributeValue('//form', 'NotExist', 'post')).to.throw(Error);
   });
 });
