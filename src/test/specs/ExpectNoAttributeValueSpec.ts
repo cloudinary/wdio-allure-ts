8bb9b15c82dc4bbf290d0d326e6a617ccf91d2a9
Ad1B3nAr0ya <adi@cloudinary.com>
2019-07-22 11:16:23 +0300

    Added tests for expectNoAttributeValue (#106)
    * added tests for expectNoAttributeValue
    
    * cr changes
    
    * fix
    
    * removed test
    
    * fix
    
    * cr changes

--- src/test/specs/ExpectNoAttributeValueSpec.ts
+++ src/test/specs/ExpectNoAttributeValueSpec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { BrowserUtils } from '../../commons/BrowserUtils';
-import { describeCommon } from '../TestHelper';
+import { describeCommon, sampleAppUrl } from '../TestHelper';
 
 const EMPTY_DIV: string = '//*[@id="formsWithoutAttribute"]//*[@id="noAttDiv"]';
 /**
@@ -8,20 +8,27 @@ const EMPTY_DIV: string = '//*[@id="formsWithoutAttribute"]//*[@id="noAttDiv"]';
  */
 describeCommon('expectNoAttributeValue', () => {
   it("Doesn't contains value", () => {
-    expect(() =>
-      BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'data-test', 'expectNoAttributeValueCent')
-    ).to.not.throw(Error);
+    expect(() => BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'data-test', 'expectNoAttributeValueCent')).to.not.throw(Error);
   });
 
   it('Contains word substring', () => {
-    expect(() =>
-      BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'data-test', 'expectNoAttributeValueCenterrr')
-    ).to.not.throw(Error);
+    expect(() => BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'data-test', 'expectNoAttributeValueCenterrr')).to.not.throw(Error);
   });
 
   it('Exact match error thrown', () => {
-    expect(() => BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'data-test', 'expectNoAttributeValueCenter')).to.throw(
-      Error
-    );
+    expect(() => BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'data-test', 'expectNoAttributeValueCenter')).to.throw(Error)
+      .with.property('message').contains("Incorrect attribute");
+  });
+
+  it('Element not exists', () => {
+    BrowserUtils.navigateToUrl(sampleAppUrl);
+    expect(() => BrowserUtils.expectNoAttributeValue('//NotExist', 'method', 'post')).to.throw(Error).with.property('message')
+      .contains("Element not exist");
+  });
+
+  it('Attribute not exists', () => {
+    BrowserUtils.navigateToUrl(sampleAppUrl);
+    expect(() => BrowserUtils.expectNoAttributeValue(EMPTY_DIV, 'NotExist', 'post')).to.throw(Error).with.property('message')
+      .contains("Incorrect attribute");
   });
 });
