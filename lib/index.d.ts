d2d1ed3e00672858fa0631e9b56837e75bced8c4
Ad1B3nAr0ya <adi@cloudinary.com>
2019-07-29 14:10:10 +0300

    compilation

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { BrowserUtils } from './commons/BrowserUtils';
-import { Reporter } from './commons/Reporter';
-import { TestUtils } from './commons/TestUtils';
-import { SpecialKeys } from './enums/SpecialKeys';
-/**
- * Export Utils
- */
-export { BrowserUtils, Reporter, SpecialKeys, TestUtils };
