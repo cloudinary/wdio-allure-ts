95df61b6e95f279d29c5d9cb8c68a13d07c7083e
Ad1B3nAr0ya <adi@cloudinary.com>
2019-10-30 21:23:42 +0200

    Improved Eyes Utils

--- lib/commons/EyesUtil.d.ts
+++ lib/commons/EyesUtil.d.ts
@@ -3,6 +3,10 @@ export interface IBoundingBox {
     width: number;
     height: number;
 }
+export interface IResult {
+    _asExpected: boolean;
+    _windowId: string;
+}
 /**
  * Class wraps the Applitools util for UI or Images comparison
  */
@@ -11,24 +15,24 @@ export declare class EyesUtil {
     constructor(apiKey: string);
     /**
      * Opens the eye session
-     * @param testDesc - Run ID
-     * @param testName - Product name
+     * @param testName - Test ID
+     * @param appName - Product name
      * @param boundingBoxObj - Bounding box to screenshots
      */
-    open(testDesc: string, testName: string, boundingBoxObj?: IBoundingBox): EyesUtil;
+    open(testName: string, appName: string, boundingBoxObj?: IBoundingBox): EyesUtil;
     /**
      * Since SDK doesn't support array of elements to ignore this method should bypass that limitation
-     * @param checkDescription - Test/Step name (unique)
+     * @param checkDescription - Step name (unique)
      * @param xPaths - array of By.type objects to ignore in check
      */
-    checkWithIgnores(checkDescription: string, xPaths: string[]): EyesUtil;
+    checkWithIgnores(checkDescription: string, xPaths: string[]): boolean;
     /**
      *  Full Page screenshots including scrolling (very slow)
-     * @param checkDesc - Unique Test ID
+     * @param checkDesc - Unique Step ID
      */
-    checkPageLayout(checkDesc: string): void;
+    checkPageLayout(checkDesc: string): boolean;
     /**
-     * Close eye batch
+     * Close eye test
      */
     close(): void;
     /**
@@ -36,6 +40,10 @@ export declare class EyesUtil {
      * @param onOff - flag to turn on and off
      */
     eyeConfiguration(onOff: boolean): EyesUtil;
+    /**
+     * Set batch name for run
+     */
+    setBatchName(batchName: string): EyesUtil;
     /**
      * Set Debug Mode On
      */
