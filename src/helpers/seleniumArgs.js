0779351957cac71cef7a76f62b75efd465b915da
Felix Zilber <felix@cloudinary.com>
2019-12-08 17:11:46 +0200

    get seleniumArgs with latest chrome driver version

---
+++ src/helpers/seleniumArgs.js
@@ -0,0 +1,41 @@
+const https = require('https');
+
+function getChromeDriverVersion() {
+  return new Promise((resolve, reject) => {
+    https
+      .get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE', resp => {
+        let data = '';
+
+        resp.on('data', chunk => {
+          data += chunk;
+        });
+
+        resp.on('end', () => {
+          resolve(data);
+        });
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+exports.getSeleniumArgs = function() {
+  return getChromeDriverVersion().then(chromeDriverVersion => {
+    return {
+      seleniumArgs: {
+        drivers: {
+          chrome: {
+            version: chromeDriverVersion,
+          },
+        },
+      },
+      seleniumInstallArgs: {
+        drivers: {
+          chrome: {
+            version: chromeDriverVersion,
+          },
+        },
+      },
+    };
+  });
+};
