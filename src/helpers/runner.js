a4e2da87702ead4613c5645e309ba8fa2aab683e
nirgur <nir.gur@cloudinary.com>
2019-12-15 18:12:44 +0200

    fix path

--- src/helpers/runner.js
+++ src/helpers/runner.js
@@ -1,34 +1,36 @@
 const { getSeleniumArgs } = require('./seleniumArgs');
 const { getOptionsFromCli } = require('./wdioCli');
-const merge = require('lodash/merge');
 const Launcher = require('@wdio/cli').default;
 const fs = require('fs');
 const path = require('path');
 
-const {config, specs} = getOptionsFromCli();
+const { config, specs } = getOptionsFromCli();
 const seleniumArgs = getSeleniumArgs();
 
 //there is no option to pass a configuration object to wdio, only path to file so we need to generate a file
-const defaultConfPath = path.resolve(__dirname,'./wdio.default.conf.js');
-
-const filename = `/tmp/wdioConfig-${Date.now()}.js` ;
-
+const defaultConfPath = path.resolve(__dirname, './wdio.default.conf.js');
+const customConfigPath = config && path.resolve(process.cwd(), config);
 const flagsConfig = { ...seleniumArgs, specs };
 
-fs.writeFileSync(filename, `module.exports.config = Object.assign({}, require('${defaultConfPath}'), require('${config}'), ${JSON.stringify(flagsConfig)});`);
+//In order to pass the custom config we need to generate a temp configuration file
+const filename = `/tmp/wdioConfig-${Date.now()}.js`;
 
-console.log(JSON.stringify(require(filename).config, null, 4));
+fs.writeFileSync(
+  filename,
+  `module.exports.config = Object.assign({}, require('${defaultConfPath}') ${
+    customConfigPath ? `,require('${customConfigPath}')` : ''
+  }, ${JSON.stringify(flagsConfig)});`
+);
 
 const wdio = new Launcher(filename);
 
-wdio.run().then(
-  code => {
-    process.exit(code);
-  },
-  error => {
+wdio
+  .run()
+  .catch(error => {
     console.error('Launcher failed to start the test', JSON.stringify(error, null, 2));
-    process.exit(1);
-  }
-).finally(() => {
+    return 1;
+  })
+  .then(code => {
     fs.unlinkSync(filename);
-});
+    process.exit(code);
+  });
--- src/test/wdio.custom.conf.js
+++ src/test/wdio.custom.conf.js
@@ -1,7 +1,6 @@
 // Specific configurations for CI tests execution on Jenkins slave
 const merge = require('deepmerge');
 const wdioConf = require('./wdio.conf.js');
-const CustomService = require('/Users/felixzilber/dev/wdio-allure-ts/src/test/customService.js').default;
 
 // have main config file as default but overwrite environment specific information
 module.exports = merge(
@@ -9,7 +8,7 @@ module.exports = merge(
   {
     // services: [CustomService],
     logLevel: 'trace',
-    specs: ['/Users/felixzilber/dev/wdio-allure-ts/lib/test/specs/ExpectTextSpec.js'],
+    specs: ['lib/test/specs/ExpectTextSpec.js'],
     onPrepare: () => console.log('123'),
     afterCommand: () => console.log('456'),
   },
