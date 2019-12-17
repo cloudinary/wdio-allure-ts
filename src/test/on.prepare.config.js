55b5028ab9f674ba2bcde9778d0db082426232e1
Felix Zilber <felix@cloudinary.com>
2019-12-17 13:08:13 +0200

    more updates for cli support

--- src/test/on.prepare.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = function() {
-  console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-  console.log('============custom onPrepare============');
-  console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-};
--- src/test/wdio.conf.js
+++ src/test/wdio.conf.js
@@ -1,40 +1,46 @@
 //Set short timeout for test
 process.env.DEFAULT_TIME_OUT = '10000';
+const wdioDefaultConf = require('../helpers/wdio.default.conf');
+const merge = require('lodash/merge');
 
-exports.config = {
-  suites: {
-    regression: ['./lib/test/specs/*Spec.js'],
+exports.config = merge(
+  wdioDefaultConf.config,
+  {
+    suites: {
+      regression: ['./lib/test/specs/*Spec.js'],
+    },
+
+    // ===================
+    // Test Configurations
+    // ===================
+    // Default timeout for all waitFor* commands.
+    waitforTimeout: 3000,
+    //
+    // Default timeout in milliseconds for request
+    // if Selenium Grid doesn't send response
+    // connectionRetryTimeout: 10000,
+    //
+    // Default request retries count
+    // connectionRetryCount: 3,
+
+    framework: 'mocha',
+    //
+
+    /**
+     * webdriver service
+     */
+    services: ['selenium-standalone'],
+
+    //Logger level Options: trace | debug | info | warn | error | silent
+    logLevel: 'silent',
+
+    //
+    // Options to be passed to Mocha.
+    // See the full list at http://mochajs.org/
+    mochaOpts: {
+      // ui: 'bdd',
+      timeout: 300000, // test timeout, test will fall after 5 minutes on timeout
+    },
   },
-
-  // ===================
-  // Test Configurations
-  // ===================
-  // Default timeout for all waitFor* commands.
-  waitforTimeout: 3000,
-  //
-  // Default timeout in milliseconds for request
-  // if Selenium Grid doesn't send response
-  // connectionRetryTimeout: 10000,
-  //
-  // Default request retries count
-  // connectionRetryCount: 3,
-
-  framework: 'mocha',
-  //
-
-  /**
-   * webdriver service
-   */
-  services: ['selenium-standalone'],
-
-  //Logger level Options: trace | debug | info | warn | error | silent
-  logLevel: 'silent',
-
-  //
-  // Options to be passed to Mocha.
-  // See the full list at http://mochajs.org/
-  mochaOpts: {
-    // ui: 'bdd',
-    timeout: 300000, // test timeout, test will fall after 5 minutes on timeout
-  },
-};
+  { clone: false }
+);
