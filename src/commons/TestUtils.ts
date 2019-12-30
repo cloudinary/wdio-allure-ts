f2e23ae92f51ba3076a0ca8b9c7148461eadde77
Felix Zilber <felix@cloudinary.com>
2019-12-30 12:05:17 +0200

    delete-escape-meta-characters-method

--- src/commons/TestUtils.ts
+++ src/commons/TestUtils.ts
@@ -7,10 +7,9 @@ export namespace TestUtils {
    * @param length length of returned string
    */
   export function randomString(length: number): string {
-    let text: string = '';
     const possible: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    text = Array(length)
+    return Array(length)
       .join()
       .split(',')
       .map(() =>
@@ -18,45 +17,5 @@ export namespace TestUtils {
         possible.charAt(Math.floor(Math.random() * possible.length))
       )
       .join('');
-
-    return text;
-  }
-
-  /**
-   * Elastic search query prohibits special chars without escaping
-   * More info: https://discuss.elastic.co/t/how-to-index-special-characters-and-search-those-special-characters-in-elasticsearch/42506
-   * Escaping special characters that are part of the query syntax. The current list special characters are: + - && || ! ( ) { } [ ] ^ " ~ * ? : \
-   * To escape these character use the \ before the character
-   * @param inputString - string that might contain special characters that needs escaping
-   */
-  export function escapeMetaCharacters(inputString: string): string {
-    const metaCharacters: string[] = [
-      '\\',
-      '^',
-      '$',
-      '{',
-      '}',
-      '[',
-      ']',
-      '(',
-      ')',
-      '*',
-      '+',
-      '?',
-      '|',
-      '<',
-      '>',
-      '-',
-      '&',
-      '%',
-    ];
-
-    for (const char of metaCharacters) {
-      if (inputString.includes(char)) {
-        inputString.replace(char, `\/${char}`);
-      }
-    }
-
-    return inputString;
   }
 }
