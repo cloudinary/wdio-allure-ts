1a642a7524cbbd6c9a9d85058f5493e9b910b589
alexeykagansky <alexey.kagansky@cloudinary.com>
2020-01-02 09:20:56 +0200

    adding checking area function for aplitools

--- lib/commons/EyesUtil.d.ts
+++ lib/commons/EyesUtil.d.ts
@@ -11,7 +11,7 @@ export interface IResult {
  * Class wraps the Applitools util for UI or Images comparison
  */
 export declare class EyesUtil {
-    public eyes: Eyes;
+    eyes: Eyes;
     constructor(apiKey: string);
     /**
      * Opens the eye session
@@ -19,39 +19,33 @@ export declare class EyesUtil {
      * @param appName - Product name
      * @param boundingBoxObj - Bounding box to screenshots
      */
-    public   open(testName: string, appName: string, boundingBoxObj?: IBoundingBox): EyesUtil;
+    open(testName: string, appName: string, boundingBoxObj?: IBoundingBox): EyesUtil;
     /**
      * Since SDK doesn't support array of elements to ignore this method should bypass that limitation
      * @param checkDescription - Step name (unique)
      * @param xPaths - array of By.type objects to ignore in check
      */
-    public  checkWithIgnores(checkDescription: string, xPaths: string[]): boolean;
+    checkWithIgnores(checkDescription: string, xPaths: string[]): boolean;
     /**
      *  Full Page screenshots including scrolling (very slow)
      * @param checkDesc - Unique Step ID
      */
-    public  checkPageLayout(checkDesc: string): boolean;
+    checkPageLayout(checkDesc: string): boolean;
     /**
      * Close eye test
      */
-    public close(): void;
+    close(): void;
     /**
      *  Set basic configuration
      * @param onOff - flag to turn on and off
      */
-    public   eyeConfiguration(onOff: boolean): EyesUtil;
+    eyeConfiguration(onOff: boolean): EyesUtil;
     /**
      * Set batch name for run
      */
-    public  setBatchName(batchName: string): EyesUtil;
+    setBatchName(batchName: string): EyesUtil;
     /**
      * Set Debug Mode On
      */
-    public   setEyeDebugMode(): EyesUtil;
-    /**
-     * checking only element area
-     * @param fileName unique name of file
-     * @param selector for checking
-     */
-    public checkElement(fileName: string, selector: string): boolean;
+    setEyeDebugMode(): EyesUtil;
 }
