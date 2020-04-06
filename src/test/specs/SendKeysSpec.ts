a541fff2c9ed1308d74194fb2361b29d11304d31
FeL <33285268+FelixZilber@users.noreply.github.com>
2020-04-06 23:31:53 +0300

    send-keys-function-update (#147)
    fix sendKeys function and update tests

--- src/test/specs/SendKeysSpec.ts
+++ src/test/specs/SendKeysSpec.ts
@@ -9,25 +9,30 @@ const text: string = 'Cloudinary';
 /**
  * wdio-allure-ts sendKeys tests
  */
-describeCommon('Send Keys', () => {
-  it('String array ', () => {
+describeCommon('SendKeysSpec', () => {
+  beforeEach(() => {
     $(TEXT_INPUT).clearValue();
     $(TEXT_INPUT).click();
+  });
+
+  it('send string', () => {
     BrowserUtils.sendKeys(text);
     assert.equal($(TEXT_INPUT).getValue(), text);
   });
 
-  it('Add text', () => {
-    $(TEXT_INPUT).clearValue();
-    $(TEXT_INPUT).click();
+  it('send an array of strings', () => {
+    const strArray: string[] = ['C', 'L', 'O', 'U', 'D', 'I', 'N', 'A', 'R', 'Y'];
+    BrowserUtils.sendKeys(strArray);
+    assert.equal($(TEXT_INPUT).getValue(), strArray.join(''));
+  });
+
+  it('send string twice', () => {
     BrowserUtils.sendKeys(text);
     BrowserUtils.sendKeys(text);
     assert.equal($(TEXT_INPUT).getValue(), `${text}${text}`);
   });
 
   it('Add text after sending enter for new line', () => {
-    $(TEXT_INPUT).clearValue();
-    $(TEXT_INPUT).click();
     BrowserUtils.sendKeys(text);
     BrowserUtils.sendKeys(SpecialKeys.ENTER);
     BrowserUtils.sendKeys(text);
@@ -36,20 +41,12 @@ describeCommon('Send Keys', () => {
 
   it('Array of Special Keys', () => {
     const specialKeysArray: SpecialKeys[] = [SpecialKeys.ENTER, SpecialKeys.ENTER, SpecialKeys.ENTER];
-    $(TEXT_INPUT).clearValue();
-    $(TEXT_INPUT).click();
-    BrowserUtils.sendKeys(text);
     BrowserUtils.sendKeys(specialKeysArray);
-    BrowserUtils.sendKeys(text);
-    assert.equal($(TEXT_INPUT).getValue(), `${text}\n\n\n${text}`);
+    assert.equal($(TEXT_INPUT).getValue(), '\n\n\n');
   });
 
   it('Special Key Emoji', () => {
-    $(TEXT_INPUT).clearValue();
-    $(TEXT_INPUT).click();
     BrowserUtils.sendKeys(SpecialKeys.EMOJI_HEART);
-    BrowserUtils.sendKeys(SpecialKeys.ENTER);
-    BrowserUtils.sendKeys(SpecialKeys.EMOJI_HEART);
-    assert.equal($(TEXT_INPUT).getValue(), `${SpecialKeys.EMOJI_HEART}\n${SpecialKeys.EMOJI_HEART}`);
+    assert.equal($(TEXT_INPUT).getValue(), SpecialKeys.EMOJI_HEART);
   });
 });
