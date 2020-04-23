e09ddd0f418e752a99dbc012cb091c1ddd564eed
FeL <33285268+FelixZilber@users.noreply.github.com>
2020-04-23 10:36:00 +0300

    GetSystemData (#153)

---
+++ src/test/specs/GetSystemDataSpec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { BrowserUtils } from '../..';
+
+describe('GetSystemData', () => {
+  it('correct data', () => {
+    expect(BrowserUtils.getSystemData()).to.contain('HeadlessChrome');
+  });
+});
