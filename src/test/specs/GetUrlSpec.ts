71704b6f9effd600056626f906dfe3fa71e5749e
FeL <33285268+FelixZilber@users.noreply.github.com>
2020-04-26 19:20:00 +0300

    GetUrlSpec (#154)

---
+++ src/test/specs/GetUrlSpec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { BrowserUtils } from '../..';
+import { describeCommon, sampleAppUrl } from '../TestHelper';
+
+describeCommon('GetUrlSpec', () => {
+  it('get url', () => {
+    expect(BrowserUtils.getUrl()).to.equal(sampleAppUrl);
+  });
+});
