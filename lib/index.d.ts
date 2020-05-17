ffa9f6f1c1a8903d8b005058496214febf32fa28
Felix Zilber <felix@cloudinary.com>
2020-05-18 00:06:29 +0300

    delete lib folder

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { BrowserUtils } from './commons/BrowserUtils';
-import { EyesUtil } from './commons/EyesUtil';
-import { Reporter } from './commons/Reporter';
-import { TestUtils } from './commons/TestUtils';
-import { SpecialKeys } from './enums/SpecialKeys';
-/**
- * Export Utils
- */
-export { BrowserUtils, EyesUtil, Reporter, SpecialKeys, TestUtils };
