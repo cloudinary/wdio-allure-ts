811fe0784356b5f2636a38bf9291945215523ce7
refael <refael.mahpud@cloudinary.com>
2020-10-27 09:54:52 +0200

    update selectors names

--- src/test/specs/DragAndDropSpec.ts
+++ src/test/specs/DragAndDropSpec.ts
@@ -2,8 +2,8 @@ import { assert, expect } from 'chai';
 import { BrowserUtils } from '../..';
 import { describeCommon } from '../TestHelper';
 
-const ELEMENT: string = '#draggedThumb';
-const TARGET: string = '#staticThumb';
+const ELEMENT: string = '#draggedSlider';
+const TARGET: string = '#staticSlider';
 const NOT_EXISTING_ELEMENT: string = '//*[@id="NotExistingElement_qweqwe"]';
 
 let beforeDragLocation: WebdriverIO.DragAndDropCoordinate;
