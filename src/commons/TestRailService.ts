93f8bca45b0ec66ef9e9813ae671a1b50ff5f32e
refael <refael.mahpud@cloudinary.com>
2020-11-12 10:22:38 +0200

    APPS-5750 added TestRailService.ts.ts, GitUtils.ts and TestRailApi.ts

--- src/commons/TestRailService.ts
+++ src/commons/TestRailService.ts
@@ -2,17 +2,17 @@ import { TestFields, TestRailApi } from './TestRailApi';
 import { AxiosPromise } from 'axios';
 
 /**
- * Git hooks scripts
+ * TestRail service
  */
-export namespace GitHooks {
+export namespace TestRailService {
   /**
    * Update automation field on testrail for the last merge tests files
+   * @param testIDs array of tests Ids
    */
   export async function setTestsAsAutomatedInTestrail(testIDs: string[]): Promise<void> {
     const promiseArray: AxiosPromise[] = [];
 
     for (const testId of testIDs) {
-      // @ts-ignore
       promiseArray.push(
         TestRailApi.Instance.changeTestField(
           testId,
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { EyesUtil } from './commons/EyesUtil';
 import { Reporter } from './commons/Reporter';
 import { TestUtils } from './commons/TestUtils';
 import { SpecialKeys } from './enums/SpecialKeys';
-import { GitHooks } from './commons/GitHooks';
+import { TestRailService } from './commons/TestRailService';
 import { GitUtils } from './commons/GitUtils';
 
 /**
  * Export Utils
  */
-export { BrowserUtils, EyesUtil, Reporter, SpecialKeys, TestUtils, GitHooks, GitUtils };
+export { BrowserUtils, EyesUtil, Reporter, SpecialKeys, TestUtils, TestRailService, GitUtils };
