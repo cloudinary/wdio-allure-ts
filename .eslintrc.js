277d1f860c6af2810407c45fb92208f1533e4666
Felix Zilber <felix@cloudinary.com>
2020-12-22 14:24:43 +0200

    disable no-inferrable-types rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
-    '@typescript-eslint/array-type': [
-      'error',
-      { default: 'generic' }
-    ],
+    '@typescript-eslint/no-inferrable-types': 0,
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
   },
-}
+};
