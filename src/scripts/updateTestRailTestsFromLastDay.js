3c27f7ba7824bfa4b08d208984a55af58534fccb
refael <refael.mahpud@cloudinary.com>
2021-02-01 11:54:33 +0200

    Add get last merged files and tests ids by hours to git utils

--- src/scripts/updateTestRailTestsFromLastDay.js
+++ src/scripts/updateTestRailTestsFromLastDay.js
@@ -3,6 +3,9 @@ const { TestRailUtil } = require('../commons/TestRailUtil');
 
 /**
  * Find tests files that was merged in the last 24 hours and update them on testRail.
+ * We're using git commands to find the the files that was merged in the last 24 hours,
+ * than we're searching for test files and extract the test id from the file name,
+ * and finally we're making an api cal for testRail to update all the tests to be automated
  */
 (() => {
     const lastHoursMergedTestsIds = GitUtils.getMergedTestsIdsByTime(24, TimeUnits.HOURS);
