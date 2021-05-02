1662aca5f45c253ee31489462010bf3b5ba9283b
alexeykagansky <alexey.kagansky@cloudinary.com>
2021-05-02 17:37:34 +0300

    test: remove FailedTestSpec.ts

--- src/test/specs/FailedTestSpec.ts
+++
@@ -1,7 +0,0 @@
-import { describeCommon } from '../TestHelper';
-
-describeCommon('FailedTest', () => {
-  it('Test error message', () => {
-    throw new Error('TEST ERROR MESSAGE----->');
-  });
-});
