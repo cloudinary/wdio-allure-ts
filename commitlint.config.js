1dddcb65816de13e9c4e73790a6b659545a967c5
alexeykagansky <alexey.kagansky@cloudinary.com>
2021-09-23 15:11:46 +0300

    docs: add chore param to commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']], // override config-conventional: allow upper case Jira ticket id (sentence-case)
-    'type-enum': [2, 'always', ['docs', 'feat', 'fix', 'test']],
+    "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]], // override config-conventional: allow upper case Jira ticket id (sentence-case)
+    "type-enum": [2, "always", ["docs", "feat", "fix", "test", "chore"]]
   },
 };
