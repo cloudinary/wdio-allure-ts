3821b322737d1eaa90331eb84141650de1b1f8aa
refael-m <refael.mahpud@cloudinary.com>
2021-11-10 17:16:44 +0200

    feat: add more logs to testRail script

--- src/commons/GitUtils.ts
+++ src/commons/GitUtils.ts
@@ -15,6 +15,7 @@ export namespace GitUtils {
    * Return list of all tests ids from the last merge.
    */
   export function getLastMergedTestsIds(): Set<string> {
+    console.log(`Getting last merged tests id's`);
     const params: string = '-m -1';
     const mergedTestsFiles: Set<string> = TestFilesUtils.getTestsFiles(executeGitScript(params));
     return TestFilesUtils.extractTestIdFromFiles(mergedTestsFiles);
@@ -26,6 +27,7 @@ export namespace GitUtils {
    * @param timeUnits - can be hours day's etc.
    */
   export function getMergedTestsIdsByTime(time: number, timeUnits: TimeUnits): Set<string> {
+    console.log(`Getting tests id's from last ${time} : ${timeUnits}`);
     const params: string = `--since="${time} ${timeUnits} ago"`;
     const mergedTestsFiles: Set<string> = TestFilesUtils.getTestsFiles(executeGitScript(params));
     return TestFilesUtils.extractTestIdFromFiles(mergedTestsFiles);
@@ -37,7 +39,9 @@ export namespace GitUtils {
  * @param params - command parameters
  */
 function executeGitScript(params: string): Set<string> {
+  console.log(`Executing git command with ${params} param`);
   const script: string = `git log ${params} --name-only --pretty="format:"`;
   const res: string = execSync(script, { timeout: 30000 }).toString();
+  console.log(`Git command res: ${res}`);
   return new Set<string>(res.split(/[\r\n]+/));
 }
--- src/commons/TestRailUtil.ts
+++ src/commons/TestRailUtil.ts
@@ -12,8 +12,11 @@ export namespace TestRailUtil {
    * @param testIDs array of tests Ids
    */
   export function setTestsAsAutomatedInTestrail(testIDs: Set<string>): void {
+    console.log(`About to update ${testIDs} on testrail`);
     for (const testId of testIDs) {
-      changeTestField(testId, TestFields.Automation, TestFields.Automation.fieldOptions.automated);
+      changeTestField(testId, TestFields.Automation, TestFields.Automation.fieldOptions.automated).then((res) => {
+        console.log(`Finished update test C${testId} with status ${res.status}`);
+      });
     }
   }
 
@@ -21,6 +24,7 @@ export namespace TestRailUtil {
    * Update an array of tests automation field on testrail from last merge
    */
   export function setTestsAsAutomatedInTestrailFromLastMerge(): void {
+    console.log(`Setting tests as 'Automated' from last merge`);
     const testIDs = GitUtils.getLastMergedTestsIds();
     setTestsAsAutomatedInTestrail(testIDs);
   }
@@ -30,6 +34,7 @@ export namespace TestRailUtil {
    * @param folderPath path of tests files to update
    */
   export function setTestsAsAutomatedInTestrailFromPath(folderPath?: string): void {
+    console.log(`Setting tests as 'Automated' from path ${folderPath}`);
     const testIDs = TestFilesUtils.getTestIdsFromFolder(folderPath);
     setTestsAsAutomatedInTestrail(testIDs);
   }
@@ -41,6 +46,7 @@ export namespace TestRailUtil {
    *   @param option field option number
    */
   export function changeTestField(testID: string, field: IField, option: number): AxiosPromise {
+    console.log(`Change test ${testID} ${field} field to ${option} option`);
     const data: Map<IField, number> = new Map<IField, number>();
     data[field.fieldName] = option;
     return TestRailApi.Instance.updateTestCase(testID, data);
