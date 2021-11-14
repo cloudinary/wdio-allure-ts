da9416016009e7a3a48a737dee11837a127a5f45
refael-m <refael.mahpud@cloudinary.com>
2021-11-14 15:08:16 +0200

    feat: update wdio packages to 7.16.6 from 7.7.4

--- src/test/wdio.conf.js
+++ src/test/wdio.conf.js
@@ -35,7 +35,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: 'warn',
   // Default timeout for all waitFor* commands.
   waitforTimeout: waitForTimeouts,
   //
