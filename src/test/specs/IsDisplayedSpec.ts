d2f1770f6ef15a2b0e048ef5a02c2cd54444d080
Felix Zilber <felix@cloudinary.com>
2021-12-28 11:03:37 +0200

    test: more logs

--- src/test/specs/IsDisplayedSpec.ts
+++ src/test/specs/IsDisplayedSpec.ts
@@ -1,17 +1,20 @@
 import { assert } from 'chai';
-import { BrowserUtils } from '../..';
+import { BrowserUtils, Reporter } from '../..';
 import { describeCommon } from '../TestHelper';
 
 describeCommon('IsDisplayed', () => {
   it('displayed true', () => {
+    Reporter.step('isDisplayed of displayed element');
     assert.isTrue(BrowserUtils.isDisplayed("//*[@id='IsDisplayed']//button[@id='displayed_button']"));
   });
 
   it('displayed false', () => {
+    Reporter.step('isDisplayed of hidden element');
     assert.isFalse(BrowserUtils.isDisplayed("//*[@id='IsDisplayed']//button[@id='not_displayed_button']"));
   });
 
   it('not exist element', () => {
+    Reporter.step('isDisplayed element not exist');
     assert.isFalse(BrowserUtils.isDisplayed("//*[@id='IsDisplayed']//button[@id='not_such_button']"));
   });
 });
