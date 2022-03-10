42c42c7faa48ac6b8b94d7a936064716a42c2c02
Felix Zilber <felix@cloudinary.com>
2022-03-10 17:40:29 +0200

    fix: IsEnabledSpec.ts

--- src/test/specs/IsEnabledSpec.ts
+++ src/test/specs/IsEnabledSpec.ts
@@ -1,4 +1,6 @@
-import { assert, expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils, Reporter } from '../..';
 import { TestUtils } from '../..';
 import { describeCommon } from '../TestHelper';
@@ -6,20 +8,19 @@ import { describeCommon } from '../TestHelper';
 const ENABLED_INPUT_SELECTOR: string = "//*[@id='IsEnabled']//input[@id='enabled_input']";
 const DISABLED_INPUT_SELECTOR: string = "//*[@id='IsEnabled']//input[@id='disabled_input']";
 describeCommon('isEnabled', () => {
-  it('check enabled element', () => {
-    Reporter.step('isEnabled of enabled element');
-    assert.isTrue(BrowserUtils.isEnabled(ENABLED_INPUT_SELECTOR));
+  it('check enabled element', async () => {
+    await Reporter.step('isEnabled of enabled element');
+    chai.assert.isTrue(await BrowserUtils.isEnabled(ENABLED_INPUT_SELECTOR));
   });
 
-  it('check disabled element', () => {
-    Reporter.step('isEnabled of disabled element');
-    assert.isFalse(BrowserUtils.isEnabled(DISABLED_INPUT_SELECTOR));
+  it('check disabled element', async () => {
+    await Reporter.step('isEnabled of disabled element');
+    chai.assert.isFalse(await BrowserUtils.isEnabled(DISABLED_INPUT_SELECTOR));
   });
-  it('check not existing element', () => {
-    Reporter.step('isEnabled of not existing element');
-    expect(() => BrowserUtils.isEnabled(`//*[@id='${TestUtils.randomString(5)}']`))
-      .to.throw(Error)
-      .with.property('message')
-      .contains("element wasn't found");
+  it('check not existing element', async () => {
+    await Reporter.step('isEnabled of not existing element');
+    await chai
+      .expect(BrowserUtils.isEnabled(`//*[@id='${TestUtils.randomString(5)}']`))
+      .to.rejectedWith(Error, "element wasn't found");
   });
 });
