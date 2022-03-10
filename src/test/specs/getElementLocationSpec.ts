7ac83bbdb151a9536025e049d91019bb050aeb0d
Felix Zilber <felix@cloudinary.com>
2022-03-10 16:45:58 +0200

    fix: getElementLocationSpec.ts

--- src/test/specs/getElementLocationSpec.ts
+++ src/test/specs/getElementLocationSpec.ts
@@ -10,12 +10,12 @@ let location: { x?: number; y?: number };
  * wdio-allure-ts get element location
  */
 describeCommon('getLocation', () => {
-  it('get element Location', () => {
+  it('get element Location', async () => {
     const expectedX: number = 22;
     const expectedY: number = 8;
 
-    Reporter.step('validate get location returned value');
-    location = BrowserUtils.getLocation(ELEMENT);
+    await Reporter.step('validate get location returned value');
+    location = await BrowserUtils.getLocation(ELEMENT);
     assert.equal(location.x, expectedX, 'Element X position');
     assert.equal(location.y, expectedY, 'Element Y position');
   });
