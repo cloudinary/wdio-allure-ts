9bda9753783d8017e24c1154190e859d71db5b0c
Felix Zilber <felix@cloudinary.com>
2022-03-10 18:35:44 +0200

    fix: WaitForClickableSpec.ts

--- src/test/specs/WaitForClickableSpec.ts
+++ src/test/specs/WaitForClickableSpec.ts
@@ -1,38 +1,39 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils } from '../../commons/BrowserUtils';
 import { describeCommon } from '../TestHelper';
 import { Reporter } from '../../index';
 
 describeCommon('waitForClickable', () => {
-  it('not clickable element', () => {
-    Reporter.step('not clickable element throws an error');
-    expect(() => BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='not_clickable_button']"))
-      .to.throw(Error)
-      .with.property('message')
-      .contains('Timeout waiting for element ');
+  it('not clickable element', async () => {
+    await Reporter.step('not clickable element throws an error');
+    await chai
+      .expect(BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='not_clickable_button']"))
+      .to.rejectedWith(Error, 'Timeout waiting for element ');
   });
 
-  it('clickable element', () => {
-    Reporter.step(' clickable element');
-    BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='clickable_button']");
+  it('clickable element', async () => {
+    await Reporter.step(' clickable element');
+    await BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='clickable_button']");
   });
-  it('reverse: true', () => {
-    Reporter.step(' clickable element - revers true');
-    BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='not_clickable_button']", { reverse: true });
+  it('reverse: true', async () => {
+    await Reporter.step(' clickable element - revers true');
+    await BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='not_clickable_button']", {
+      reverse: true,
+    });
   });
 
-  it('custom options', () => {
-    Reporter.step(' clickable element - custom options');
-    expect(() =>
-      BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='not_clickable_button']", {
-        timeout: 1234,
-        timeoutMsg: 'custom message',
-        interval: 1000,
-      })
-    )
-      .to.throw(Error)
-      .with.property('message')
-      .contains('Timeout waiting for element ')
-      .contains('custom message');
+  it('custom options', async () => {
+    await Reporter.step(' clickable element - custom options');
+    await chai
+      .expect(
+        BrowserUtils.waitForClickable("//*[@id='waitForClickable']//button[@id='not_clickable_button']", {
+          timeout: 1234,
+          timeoutMsg: 'custom message',
+          interval: 1000,
+        })
+      )
+      .to.rejectedWith(Error, 'custom message');
   });
 });
