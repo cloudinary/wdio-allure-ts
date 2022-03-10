9f0520655331071efb73ce3fbdd0df11c0ad36e8
Felix Zilber <felix@cloudinary.com>
2022-03-10 17:41:29 +0200

    fix: RefreshBrowserSpec.ts.ts

--- src/test/specs/RefreshBrowserSpec.ts
+++ src/test/specs/RefreshBrowserSpec.ts
@@ -5,23 +5,23 @@ import { describeCommon } from '../TestHelper';
 const REFRESH_BROWSER_FIELD: string = "//*[@id='RefreshBrowserSpec']//*[@id='RefreshBrowserCheckFiled']";
 
 describeCommon('RefreshSpec', () => {
-  it('RefreshTrue', () => {
-    Reporter.step('scroll to refresh field');
-    $(REFRESH_BROWSER_FIELD).scrollIntoView();
+  it('RefreshTrue', async () => {
+    await Reporter.step('scroll to refresh field');
+    await $(REFRESH_BROWSER_FIELD).scrollIntoView();
 
-    Reporter.step('set text value');
-    $(REFRESH_BROWSER_FIELD).setValue('TestText');
+    await Reporter.step('set text value');
+    await $(REFRESH_BROWSER_FIELD).setValue('TestText');
 
-    Reporter.step('refresh browser');
-    BrowserUtils.refresh();
+    await Reporter.step('refresh browser');
+    await BrowserUtils.refresh();
 
-    Reporter.step('scroll into view');
-    $(REFRESH_BROWSER_FIELD).scrollIntoView();
+    await Reporter.step('scroll into view');
+    await $(REFRESH_BROWSER_FIELD).scrollIntoView();
 
-    Reporter.step('wait for field to be displayed');
-    $(REFRESH_BROWSER_FIELD).waitForDisplayed();
+    await Reporter.step('wait for field to be displayed');
+    await $(REFRESH_BROWSER_FIELD).waitForDisplayed();
 
-    Reporter.step('validate field text');
-    expect($(REFRESH_BROWSER_FIELD).getValue()).to.be.eq('');
+    await Reporter.step('validate field text');
+    expect(await $(REFRESH_BROWSER_FIELD).getValue()).to.be.eq('');
   });
 });
