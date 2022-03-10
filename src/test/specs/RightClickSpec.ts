94c6f35c9294dad0821b95cd2270942cd8c32dcd
Felix Zilber <felix@cloudinary.com>
2022-03-10 17:42:11 +0200

    fix: RightClickSpec.ts

--- src/test/specs/RightClickSpec.ts
+++ src/test/specs/RightClickSpec.ts
@@ -8,11 +8,11 @@ const RIGHT_CLICK_BUTTON_SELECTOR: string = "//*[@id='buttonRightMouseClick']";
  * wdio-allure-ts Right Click actions on element test
  */
 describeCommon('RightClickSpec', () => {
-  it('RightClick ', () => {
-    Reporter.step('Right click on button');
-    BrowserUtils.click(RIGHT_CLICK_BUTTON_SELECTOR, { button: 'right' });
+  it('RightClick ', async () => {
+    await Reporter.step('Right click on button');
+    await BrowserUtils.click(RIGHT_CLICK_BUTTON_SELECTOR, { button: 'right' });
 
-    Reporter.step(`Validate right click execution`);
-    assert.equal(BrowserUtils.getText(RIGHT_CLICK_BUTTON_SELECTOR), 'Button clicked');
+    await Reporter.step(`Validate right click execution`);
+    assert.equal(await BrowserUtils.getText(RIGHT_CLICK_BUTTON_SELECTOR), 'Button clicked');
   });
 });
