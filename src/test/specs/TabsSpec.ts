974a48c868d0b6d57ef2ca0eedcd6b4814b52f8f
Felix Zilber <felix@cloudinary.com>
2022-03-10 18:19:59 +0200

    fix: TabsSpec.ts

--- src/test/specs/TabsSpec.ts
+++ src/test/specs/TabsSpec.ts
@@ -1,4 +1,6 @@
-import { assert, expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils, Reporter } from '../..';
 import { describeCommon, sampleAppUrl } from '../TestHelper';
 
@@ -7,46 +9,45 @@ const OPEN_TAB_BUTTON: string = "//button[@data-test='open-tab-btn']";
  * wdio-allure-ts tab actions test
  */
 describeCommon('tab actions', () => {
-  it('get tab ids', () => {
-    Reporter.step('get tab ids');
-    let tabIds: Array<string> = BrowserUtils.getWindowHandles();
+  it('get tab ids', async () => {
+    await Reporter.step('get tab ids');
+    let tabIds: Array<string> = await BrowserUtils.getWindowHandles();
 
-    Reporter.step('validate number of tabs');
-    assert.equal(tabIds.length, 1, 'Incorrect number of tab ids');
+    await Reporter.step('validate number of tabs');
+    chai.assert.equal(tabIds.length, 1, 'Incorrect number of tab ids');
 
-    Reporter.step('open new tab');
-    BrowserUtils.click(OPEN_TAB_BUTTON);
+    await Reporter.step('open new tab');
+    await BrowserUtils.click(OPEN_TAB_BUTTON);
 
-    Reporter.step('get tab ids');
-    tabIds = BrowserUtils.getWindowHandles();
+    await Reporter.step('get tab ids');
+    tabIds = await BrowserUtils.getWindowHandles();
 
-    Reporter.step('validate number of tabs');
-    assert.equal(tabIds.length, 2, 'Incorrect number of tab ids');
+    await Reporter.step('validate number of tabs');
+    chai.assert.equal(tabIds.length, 2, 'Incorrect number of tab ids');
   });
 
-  it('switch to tab', () => {
-    Reporter.step('Navigate to sample app');
-    BrowserUtils.url(sampleAppUrl);
+  it('switch to tab', async () => {
+    await Reporter.step('Navigate to sample app');
+    await BrowserUtils.url(sampleAppUrl);
 
-    Reporter.step('open new tab');
-    BrowserUtils.click(OPEN_TAB_BUTTON);
+    await Reporter.step('open new tab');
+    await BrowserUtils.click(OPEN_TAB_BUTTON);
 
-    Reporter.step('get tabs ids');
-    const tabIds: Array<string> = BrowserUtils.getWindowHandles();
+    await Reporter.step('get tabs ids');
+    const tabIds: Array<string> = await BrowserUtils.getWindowHandles();
 
-    Reporter.step('switch to tab');
-    BrowserUtils.switchToWindow(tabIds[1]);
+    await Reporter.step('switch to tab');
+    await BrowserUtils.switchToWindow(tabIds[1]);
 
-    Reporter.step('Validate tab change');
-    assert.equal(tabIds[1], browser.getWindowHandle(), 'Failed to switch tabs');
+    await Reporter.step('Validate tab change');
+    chai.assert.equal(tabIds[1], await browser.getWindowHandle(), 'Failed to switch tabs');
   });
 
-  it('switch to incorrect window id', () => {
+  it('switch to incorrect window id', async () => {
     const badTabId: string = 'notExistingTabId';
-    Reporter.step('switch to incorrect window id throws an error');
-    expect(() => BrowserUtils.switchToWindow(badTabId))
-      .to.throw(Error)
-      .with.property('message')
-      .contains(`Failed switch to window by id: '${badTabId}'`);
+    await Reporter.step('switch to incorrect window id throws an error');
+    await chai
+      .expect(BrowserUtils.switchToWindow(badTabId))
+      .to.rejectedWith(Error, `Failed switch to window by id: '${badTabId}'`);
   });
 });
