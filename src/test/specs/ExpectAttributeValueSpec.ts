b95b3ead59d1c690fef7da6d135d5c7feaa4073d
Felix Zilber <felix@cloudinary.com>
2022-03-10 18:31:29 +0200

    fix: ExpectAttributeValueSpec.ts

--- src/test/specs/ExpectAttributeValueSpec.ts
+++ src/test/specs/ExpectAttributeValueSpec.ts
@@ -1,4 +1,6 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils, Reporter } from '../..';
 import { describeCommon } from '../TestHelper';
 
@@ -13,34 +15,29 @@ const INCORRECT_ATTRIBUTE_VALUE: string = 'hello hello world';
  * wdio-allure-ts waitForAttributeValueSpec
  */
 describeCommon('waitForAttributeValue', () => {
-  it('correct value', () => {
-    Reporter.step('Wait for attribute value');
-    expect(() =>
-      BrowserUtils.waitForAttributeValue(SELECTOR_WITH_ATTRIBUTE, ATTRIBUTE_NAME, CORRECT_ATTRIBUTE_VALUE)
-    ).to.not.throw(Error);
+  it('correct value', async () => {
+    await Reporter.step('Wait for attribute value');
+    await BrowserUtils.waitForAttributeValue(SELECTOR_WITH_ATTRIBUTE, ATTRIBUTE_NAME, CORRECT_ATTRIBUTE_VALUE);
   });
 
-  it('incorrect value', () => {
-    Reporter.step('Wait for Incorrect attribute value throws an error');
-    expect(() => BrowserUtils.waitForAttributeValue(SELECTOR_WITH_ATTRIBUTE, ATTRIBUTE_NAME, INCORRECT_ATTRIBUTE_VALUE))
-      .to.throw(Error)
-      .with.property('message')
-      .contains(`Incorrect attribute`);
+  it('incorrect value', async () => {
+    await Reporter.step('Wait for Incorrect attribute value throws an error');
+    await chai
+      .expect(BrowserUtils.waitForAttributeValue(SELECTOR_WITH_ATTRIBUTE, ATTRIBUTE_NAME, INCORRECT_ATTRIBUTE_VALUE))
+      .to.rejectedWith(Error, `Incorrect attribute`);
   });
-  it('incorrect selector', () => {
-    Reporter.step('Wait for attribute value with incorrect selector throws an error');
-    expect(() => BrowserUtils.waitForAttributeValue(INCORRECT_SELECTOR, ATTRIBUTE_NAME, INCORRECT_ATTRIBUTE_VALUE))
-      .to.throw(Error)
-      .with.property('message')
-      .contains(`Incorrect attribute`);
+  it('incorrect selector', async () => {
+    await Reporter.step('Wait for attribute value with incorrect selector throws an error');
+    await chai
+      .expect(BrowserUtils.waitForAttributeValue(INCORRECT_SELECTOR, ATTRIBUTE_NAME, INCORRECT_ATTRIBUTE_VALUE))
+      .to.rejectedWith(Error, `Incorrect attribute`);
   });
-  it('incorrect attribute', () => {
-    Reporter.step('Wait for value of incorrect attribute throws an error');
-    expect(() =>
-      BrowserUtils.waitForAttributeValue(INCORRECT_SELECTOR, INCORRECT_ATTRIBUTE_NAME, INCORRECT_ATTRIBUTE_VALUE)
-    )
-      .to.throw(Error)
-      .with.property('message')
-      .contains(`Incorrect attribute`);
+  it('incorrect attribute', async () => {
+    await Reporter.step('Wait for value of incorrect attribute throws an error');
+    await chai
+      .expect(
+        BrowserUtils.waitForAttributeValue(INCORRECT_SELECTOR, INCORRECT_ATTRIBUTE_NAME, INCORRECT_ATTRIBUTE_VALUE)
+      )
+      .to.rejectedWith(Error, `Incorrect attribute`);
   });
 });
