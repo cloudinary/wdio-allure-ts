3eb1887f9647d40178f11d15fe50d2db2d0e3e70
Felix Zilber <felix@cloudinary.com>
2022-03-10 17:48:09 +0200

    fix: ScrollToSpec.ts

--- src/test/specs/ScrollToSpec.ts
+++ src/test/specs/ScrollToSpec.ts
@@ -6,32 +6,32 @@ const TEST_DIV_ID: string = 'scrollToPixelsDiv';
 const TEST_FIELD_SELECTOR: string = `//*[@id='${TEST_DIV_ID}']`;
 
 describeCommon('ScrollTo', () => {
-  it('scroll to y value', () => {
+  it('scroll to y value', async () => {
     const scrollToYValue: number = 50;
-    Reporter.step('scroll to y value');
-    BrowserUtils.scrollTo(TEST_FIELD_SELECTOR, 0, scrollToYValue);
+    await Reporter.step('scroll to y value');
+    await BrowserUtils.scrollTo(TEST_FIELD_SELECTOR, 0, scrollToYValue);
 
-    Reporter.step('validate correct position');
-    assert.isTrue(getVerticalPosition() === scrollToYValue);
+    await Reporter.step('validate correct position');
+    assert.isTrue((await getVerticalPosition()) === scrollToYValue);
   });
-  it('scroll to x value', () => {
+  it('scroll to x value', async () => {
     const scrollToXValue: number = 50;
-    Reporter.step('scroll to x value');
-    BrowserUtils.scrollTo(TEST_FIELD_SELECTOR, scrollToXValue, 0);
+    await Reporter.step('scroll to x value');
+    await BrowserUtils.scrollTo(TEST_FIELD_SELECTOR, scrollToXValue, 0);
 
-    Reporter.step('validate correct position');
-    assert.isTrue(getHorizontalPosition() === scrollToXValue);
+    await Reporter.step('validate correct position');
+    assert.isTrue((await getHorizontalPosition()) === scrollToXValue);
   });
 });
 
-function getVerticalPosition(): number {
-  Reporter.step('get vertical position');
+async function getVerticalPosition(): Promise<number> {
+  await Reporter.step('get vertical position');
   const script: string = `return document.getElementById('${TEST_DIV_ID}').scrollTop`;
-  return Number(BrowserUtils.execute(script));
+  return Number(await BrowserUtils.execute(script));
 }
 
-function getHorizontalPosition(): number {
-  Reporter.step('get horizontal position');
+async function getHorizontalPosition(): Promise<number> {
+  await Reporter.step('get horizontal position');
   const script: string = `return document.getElementById('${TEST_DIV_ID}').scrollLeft`;
-  return Number(BrowserUtils.execute(script));
+  return Number(await BrowserUtils.execute(script));
 }
