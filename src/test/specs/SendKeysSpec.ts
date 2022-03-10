ef8a4013ac23840f04f0ed1e6434b533fd99eae4
Felix Zilber <felix@cloudinary.com>
2022-03-10 17:53:03 +0200

    fix: SendKeysSpec.ts

--- src/test/specs/SendKeysSpec.ts
+++ src/test/specs/SendKeysSpec.ts
@@ -10,70 +10,70 @@ const text: string = 'Cloudinary';
  * wdio-allure-ts keys tests
  */
 describeCommon('SendKeysSpec', () => {
-  beforeEach(() => {
-    Reporter.step('clear text input');
-    $(TEXT_INPUT).clearValue();
+  beforeEach(async () => {
+    await Reporter.step('clear text input');
+    await $(TEXT_INPUT).clearValue();
 
-    Reporter.step('click text input');
-    $(TEXT_INPUT).click();
+    await Reporter.step('click text input');
+    await $(TEXT_INPUT).click();
   });
 
-  it('send string', () => {
-    Reporter.step('send keys - string');
-    BrowserUtils.keys(text);
+  it('send string', async () => {
+    await Reporter.step('send keys - string');
+    await BrowserUtils.keys(text);
 
-    Reporter.step('Validate text value');
-    assert.equal($(TEXT_INPUT).getValue(), text);
+    await Reporter.step('Validate text value');
+    assert.equal(await $(TEXT_INPUT).getValue(), text);
   });
 
-  it('send an array of strings', () => {
+  it('send an array of strings', async () => {
     const strArray: Array<string> = ['C', 'L', 'O', 'U', 'D', 'I', 'N', 'A', 'R', 'Y'];
-    Reporter.step('send keys - string array');
-    BrowserUtils.keys(strArray);
+    await Reporter.step('send keys - string array');
+    await BrowserUtils.keys(strArray);
 
-    Reporter.step('Validate text value');
-    assert.equal($(TEXT_INPUT).getValue(), strArray.join(''));
+    await Reporter.step('Validate text value');
+    assert.equal(await $(TEXT_INPUT).getValue(), strArray.join(''));
   });
 
-  it('send string twice', () => {
-    Reporter.step('send keys - string');
-    BrowserUtils.keys(text);
+  it('send string twice', async () => {
+    await Reporter.step('send keys - string');
+    await BrowserUtils.keys(text);
 
-    Reporter.step('send keys - string');
-    BrowserUtils.keys(text);
+    await Reporter.step('send keys - string');
+    await BrowserUtils.keys(text);
 
-    Reporter.step('Validate text value');
-    assert.equal($(TEXT_INPUT).getValue(), `${text}${text}`);
+    await Reporter.step('Validate text value');
+    assert.equal(await $(TEXT_INPUT).getValue(), `${text}${text}`);
   });
 
-  it('Add text after sending enter for new line', () => {
-    Reporter.step('send keys - string');
-    BrowserUtils.keys(text);
+  it('Add text after sending enter for new line', async () => {
+    await Reporter.step('send keys - string');
+    await BrowserUtils.keys(text);
 
-    Reporter.step('send keys - ENTER');
-    BrowserUtils.keys(SpecialKeys.ENTER);
+    await Reporter.step('send keys - ENTER');
+    await BrowserUtils.keys(SpecialKeys.ENTER);
 
-    Reporter.step('send keys - string');
-    BrowserUtils.keys(text);
+    await Reporter.step('send keys - string');
+    await BrowserUtils.keys(text);
 
-    Reporter.step('Validate text value');
-    assert.equal($(TEXT_INPUT).getValue(), `${text}\n${text}`);
+    await Reporter.step('Validate text value');
+    assert.equal(await $(TEXT_INPUT).getValue(), `${text}\n${text}`);
   });
 
-  it('Array of Special Keys', () => {
+  it('Array of Special Keys', async () => {
     const specialKeysArray: Array<SpecialKeys> = [SpecialKeys.ENTER, SpecialKeys.ENTER, SpecialKeys.ENTER];
-    Reporter.step('send keys - string array special chars');
-    BrowserUtils.keys(specialKeysArray);
+    await Reporter.step('send keys - string array special chars');
+    await BrowserUtils.keys(specialKeysArray);
 
-    Reporter.step('Validate text value');
-    assert.equal($(TEXT_INPUT).getValue(), '\n\n\n');
+    await Reporter.step('Validate text value');
+    assert.equal(await $(TEXT_INPUT).getValue(), '\n\n\n');
   });
 
-  it('Special Key Emoji', () => {
-    Reporter.step('send keys - emoji');
-    BrowserUtils.keys(SpecialKeys.EMOJI_HEART);
+  it('Special Key Emoji', async () => {
+    await Reporter.step('send keys - emoji');
+    await BrowserUtils.keys(SpecialKeys.EMOJI_HEART);
 
-    Reporter.step('Validate text value');
-    assert.equal($(TEXT_INPUT).getValue(), SpecialKeys.EMOJI_HEART);
+    await Reporter.step('Validate text value');
+    assert.equal(await $(TEXT_INPUT).getValue(), SpecialKeys.EMOJI_HEART);
   });
 });
