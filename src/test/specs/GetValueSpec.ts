5d3ba103e99a480ffc0ad7e66e943970844f0038
Felix Zilber <felix@cloudinary.com>
2022-03-10 17:38:34 +0200

    fix: GetValueSpec.ts

--- src/test/specs/GetValueSpec.ts
+++ src/test/specs/GetValueSpec.ts
@@ -6,15 +6,15 @@ const INPUT_ELEMENT_SELECTOR: string = "//*[@id='getValue']//input[@id='getValue
 const TEXTAREA_ELEMENT_SELECTOR: string = "//*[@id='getValue']//textarea[@id='getValue-textarea']";
 
 describeCommon('getValue', () => {
-  it('get value of input element', () => {
+  it('get value of input element', async () => {
     const expectedInputValue = 'get value input element';
-    Reporter.step('Validate getValue returned value from input');
-    expect(BrowserUtils.getValue(INPUT_ELEMENT_SELECTOR)).to.be.eq(expectedInputValue);
+    await Reporter.step('Validate getValue returned value from input');
+    expect(await BrowserUtils.getValue(INPUT_ELEMENT_SELECTOR)).to.be.eq(expectedInputValue);
   });
 
-  it('get value of text area', () => {
+  it('get value of text area', async () => {
     const expectedTextAreaValue = 'get value text area';
-    Reporter.step('Validate getValue returned value text area');
-    expect($(TEXTAREA_ELEMENT_SELECTOR).getValue()).to.be.eq(expectedTextAreaValue);
+    await Reporter.step('Validate getValue returned value text area');
+    expect(await $(TEXTAREA_ELEMENT_SELECTOR).getValue()).to.be.eq(expectedTextAreaValue);
   });
 });
