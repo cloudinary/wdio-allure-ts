c41a38560cf5ac01c2670c707fb2ce01e3732f7e
Felix Zilber <felix@cloudinary.com>
2022-03-10 19:02:52 +0200

    fix: WaitForEnabledSpec.ts

--- src/test/specs/WaitForEnabledSpec.ts
+++ src/test/specs/WaitForEnabledSpec.ts
@@ -1,4 +1,6 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils, Reporter } from '../..';
 import { describeCommon } from '../TestHelper';
 
@@ -6,22 +8,21 @@ const ENABLED_ELEMENT_SELECTOR: string = "//button[@id='click_to_enable_second_b
 const DISABLED_ELEMENT_SELECTOR: string = "//button[@id='disabled_button']";
 const NOT_EXISTS_ELEMENT_SELECTOR: string = "//button[@id='disabled_button1234']";
 describeCommon('waitForEnabled', () => {
-  it('element enabled', () => {
-    Reporter.step('click an element');
-    BrowserUtils.click(ENABLED_ELEMENT_SELECTOR);
+  it('element enabled', async () => {
+    await Reporter.step('click an element');
+    await BrowserUtils.click(ENABLED_ELEMENT_SELECTOR);
 
-    Reporter.step('Wait for enabled');
-    expect(() => BrowserUtils.waitForEnabled(ENABLED_ELEMENT_SELECTOR)).to.not.throw(Error);
+    await Reporter.step('Wait for enabled');
+    await BrowserUtils.waitForEnabled(ENABLED_ELEMENT_SELECTOR);
   });
-  it('element disabled', () => {
-    Reporter.step('disabled element throws an error');
-    expect(() => BrowserUtils.waitForEnabled(DISABLED_ELEMENT_SELECTOR))
-      .to.throw(Error)
-      .with.property('message')
-      .contains(`Element not enabled`);
+  it('element disabled', async () => {
+    await Reporter.step('disabled element throws an error');
+    await chai
+      .expect(BrowserUtils.waitForEnabled(DISABLED_ELEMENT_SELECTOR))
+      .to.rejectedWith(Error, `Element not enabled`);
   });
-  it('element not exists', () => {
-    Reporter.step('not existing element throws an error');
-    expect(() => BrowserUtils.waitForEnabled(NOT_EXISTS_ELEMENT_SELECTOR)).to.throw(Error);
+  it('element not exists', async () => {
+    await Reporter.step('not existing element throws an error');
+    await chai.expect(BrowserUtils.waitForEnabled(NOT_EXISTS_ELEMENT_SELECTOR)).to.rejectedWith(Error);
   });
 });
