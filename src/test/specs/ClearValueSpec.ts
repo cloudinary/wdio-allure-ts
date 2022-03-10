a1e4597ad6729527a73e8df19ad55f6a4b25b4e9
Felix Zilber <felix@cloudinary.com>
2022-03-10 12:53:01 +0200

    fix: ClearValueSpec.ts

--- src/test/specs/ClearValueSpec.ts
+++ src/test/specs/ClearValueSpec.ts
@@ -1,4 +1,6 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils, Reporter } from '../..';
 import { describeCommon } from '../TestHelper';
 
@@ -7,27 +9,26 @@ const TEXT_AREA_SELECTOR: string = "//*[@id='clearValue']//textarea[@id='clearVa
 const DISABLED_INPUT_SELECTOR: string = "//*[@id='AddValue-disabled_input']";
 
 describeCommon('clearValue', () => {
-  it('clear value of text', () => {
-    Reporter.step('Clear input element value');
-    BrowserUtils.clearValue(INPUT_TEXT_SELECTOR);
+  it('clear value of text', async () => {
+    await Reporter.step('Clear input element value');
+    await BrowserUtils.clearValue(INPUT_TEXT_SELECTOR);
 
-    Reporter.step('Validate input is empty');
-    expect($(INPUT_TEXT_SELECTOR).getValue()).to.be.eq('');
+    await Reporter.step('Validate input is empty');
+    await chai.expect(await $(INPUT_TEXT_SELECTOR).getValue()).to.be.eq('');
   });
 
-  it('clear value of text area', () => {
-    Reporter.step('Validate text area value');
-    BrowserUtils.clearValue(TEXT_AREA_SELECTOR);
+  it('clear value of text area', async () => {
+    await Reporter.step('Validate text area value');
+    await BrowserUtils.clearValue(TEXT_AREA_SELECTOR);
 
-    Reporter.step('Validate text area is empty');
-    expect($(TEXT_AREA_SELECTOR).getValue()).to.be.eq('');
+    await Reporter.step('Validate text area is empty');
+    await chai.expect(await $(TEXT_AREA_SELECTOR).getValue()).to.be.eq('');
   });
 
-  it('clear value of disable input selector', () => {
-    Reporter.step('Clear value of disabled element');
-    expect(() => BrowserUtils.clearValue(DISABLED_INPUT_SELECTOR))
-      .to.throw(Error)
-      .with.property('message')
-      .contains('Failed to clear value in');
+  it('clear value of disable input selector', async () => {
+    await Reporter.step('Clear value of disabled element');
+    await chai
+      .expect(BrowserUtils.clearValue(DISABLED_INPUT_SELECTOR))
+      .to.rejectedWith(Error, 'Failed to clear value in');
   });
 });
