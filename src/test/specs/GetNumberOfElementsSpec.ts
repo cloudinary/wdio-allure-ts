6c0cfe786661dc5d675f14f61ae387c5876d0b69
Felix Zilber <felix@cloudinary.com>
2022-03-10 16:48:51 +0200

    fix: GetNumberOfElementsSpec.ts

--- src/test/specs/GetNumberOfElementsSpec.ts
+++ src/test/specs/GetNumberOfElementsSpec.ts
@@ -1,4 +1,6 @@
-import { assert, expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils, Reporter } from '../..';
 import { describeCommon } from '../TestHelper';
 
@@ -8,18 +10,22 @@ const ELEMENT_SELECTOR: string = "//*[@id='GetNumberOfElements']//*[@data-test='
  * getNumberOfElements
  */
 describeCommon('getNumberOfElements', () => {
-  it('get number of existing elements', () => {
-    Reporter.step('Validate return value of getNumberOfElements');
-    assert.equal(BrowserUtils.getNumberOfElements(ELEMENT_SELECTOR), 2, 'Incorrect number of elements');
+  it('get number of existing elements', async () => {
+    await Reporter.step('Validate return value of getNumberOfElements');
+    chai.assert.equal(await BrowserUtils.getNumberOfElements(ELEMENT_SELECTOR), 2, 'Incorrect number of elements');
   });
 
-  it('get number of not existing elements', () => {
-    Reporter.step('Validate return value of getNumberOfElements when elements not exists');
-    assert.equal(BrowserUtils.getNumberOfElements(`${ELEMENT_SELECTOR}//bla`), 0, 'Incorrect number of elements');
+  it('get number of not existing elements', async () => {
+    await Reporter.step('Validate return value of getNumberOfElements when elements not exists');
+    chai.assert.equal(
+      await BrowserUtils.getNumberOfElements(`${ELEMENT_SELECTOR}//bla`),
+      0,
+      'Incorrect number of elements'
+    );
   });
 
-  it('undefined selector value', () => {
-    Reporter.step('getNumberOfElements throws an error when selector is undefined');
-    expect(() => BrowserUtils.getNumberOfElements(undefined)).to.throw(Error);
+  it('undefined selector value', async () => {
+    await Reporter.step('getNumberOfElements throws an error when selector is undefined');
+    await chai.expect(BrowserUtils.getNumberOfElements(undefined)).to.rejectedWith(Error);
   });
 });
