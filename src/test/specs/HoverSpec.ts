33b1ffc5909f041f28acfdad7d5fe6a82810a095
Felix Zilber <felix@cloudinary.com>
2022-03-10 12:09:49 +0200

    fix: HoverSpec.ts

--- src/test/specs/HoverSpec.ts
+++ src/test/specs/HoverSpec.ts
@@ -1,25 +1,29 @@
-import { assert, expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 import { BrowserUtils, Reporter } from '../..';
 import { describeCommon } from '../TestHelper';
 
 const HOVER_BUTTON: string = "//*[@id='HoverSpec']//*[@id='HoverButton']";
-const NOT_EXISITING_ELEMENT_SELECTOR: string = "//*[@class='AndrzejTheKing']";
+const NOT_EXISTING_ELEMENT_SELECTOR: string = "//*[@class='AndrzejTheKing']";
 /**
  * wdio-allure-ts moveTo actions on test button
  */
 
 describeCommon('moveToSpec', () => {
-  it('hoverTrue', () => {
-    Reporter.step('scroll to hover button');
-    $(HOVER_BUTTON).scrollIntoView();
-    Reporter.step('move to hover button');
-    BrowserUtils.moveTo(HOVER_BUTTON);
-    Reporter.step('Validate button hovered');
-    assert.equal($(HOVER_BUTTON).getCSSProperty('background-color').parsed.hex, '#4caf50');
+  it('hoverTrue', async () => {
+    await Reporter.step('scroll to hover button');
+    await $(HOVER_BUTTON).scrollIntoView();
+
+    await Reporter.step('move to hover button');
+    await BrowserUtils.moveTo(HOVER_BUTTON);
+
+    await Reporter.step('Validate button hovered');
+    await chai.assert.equal((await $(HOVER_BUTTON).getCSSProperty('background-color')).parsed.hex, '#4caf50');
   });
 
-  it('hoverFalse', () => {
-    Reporter.step('Move to not existing element throws an error');
-    expect(() => BrowserUtils.moveTo(NOT_EXISITING_ELEMENT_SELECTOR)).to.throw(Error);
+  it('hoverFalse', async () => {
+    await Reporter.step('Move to not existing element throws an error');
+    await chai.expect(BrowserUtils.moveTo(NOT_EXISTING_ELEMENT_SELECTOR)).to.rejectedWith(Error, 'Wait for exist');
   });
 });
