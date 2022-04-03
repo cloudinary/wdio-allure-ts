2d7102e5f442ab92445bafa04fb6815baaa32661
Felix Zilber <felix@cloudinary.com>
2022-04-03 10:18:47 +0300

    fix: make getTestData async

--- src/commons/TestUtils.ts
+++ src/commons/TestUtils.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import { Reporter } from '../index';
 
 /**
  * General utils useful for test
@@ -49,7 +50,7 @@ export namespace TestUtils {
    * @param dataTag type of file
    *  configDataFilePath will take from wdio.config file
    */
-  export function getData<T>(dataTag: string = process.env.TEST_DATA_TAG): T {
+  export async function getData<T>(dataTag: string = process.env.TEST_DATA_TAG): Promise<T> {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const dataFilename = browser.config.configDataFilePath;
@@ -60,9 +61,12 @@ export namespace TestUtils {
     if (dataFilePath === undefined) {
       throw new Error('Path to data file is incorrect');
     }
+    await Reporter.debug(`Getting data from file ${dataFilePath}`);
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const data: T = require(dataFilePath);
-
+    await Reporter.debug(
+      `Received a data ${JSON.stringify(data)} from file by provided tag ${JSON.stringify(data[dataTag])}`
+    );
     return data && data[dataTag];
   }
 }
--- src/test/specs/GetTestDataFileSpec.ts
+++ src/test/specs/GetTestDataFileSpec.ts
@@ -15,14 +15,14 @@ describeCommon('GetTestDataFileSpec', () => {
   it('Check get test data from env file', async () => {
     process.env.TEST_DATA_TAG = 'test-user';
     await Reporter.step('getData reads from file');
-    const data: ITestData = TestUtils.getData();
+    const data: ITestData = await TestUtils.getData();
     assert.equal(data.fileName, 'example');
   });
 
   it('Check incorrect user', async () => {
     process.env.TEST_DATA_TAG = 'incorrect-user';
     await Reporter.step('undefined returned in case of incorrect data tag');
-    const data: ITestData = TestUtils.getData();
+    const data: ITestData = await TestUtils.getData();
     assert.equal(data, undefined);
   });
 });
