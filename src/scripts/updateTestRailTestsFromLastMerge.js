b06b0da02a673d5bf934fe3ab8d283312892e1ad
refael-m <refael.mahpud@cloudinary.com>
2022-04-14 11:37:18 +0300

    feat: add get merged tests Ids since last merge functionality

--- src/scripts/updateTestRailTestsFromLastMerge.js
+++ src/scripts/updateTestRailTestsFromLastMerge.js
@@ -3,19 +3,16 @@ require('dotenv').config();
 const { GitUtils } = require('../commons/GitUtils');
 const { TestRailUtil } = require('../commons/TestRailUtil');
 
-//if there is no DAYS_TO_COUNT_BACK env variable it's set to 1 day back
-const DAYS_TO_COUNT_BACK = Number(process.env.DAYS_TO_COUNT_BACK) || 1;
-
 /**
- * Find tests files that was merged in the last 24 hours and update them on testRail.
- * We're using git commands to find the the files that was merged in the last 24 hours,
- * than we're searching for test files and extract the test id from the file name,
+ * Find tests files that was merged in the last merge and update them on testRail.
+ * We're using git commands to find the files that was merged,
+ * then we're searching for test files and extract the test id from the file name,
  * and finally we're making an api cal for testRail to update all the tests to be automated
  */
 (async () => {
-  const testsToUpdate = GitUtils.getMergedTestsIdsSinceDay(DAYS_TO_COUNT_BACK);
+  const testsToUpdate = GitUtils.getMergedTestsIdsSinceLastMerge();
   if (!testsToUpdate || testsToUpdate.size === 0) {
-    console.log(`No tests to update since last ${DAYS_TO_COUNT_BACK} day's`);
+    console.log(`No tests to update since last merge`);
   } else {
     console.log(`Updating ${testsToUpdate.size} tests...`);
     console.log(testsToUpdate);
