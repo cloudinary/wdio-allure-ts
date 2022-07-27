60bc50f0941993e8260a557a2bd7de4a3f31bdbf
nirtatuani <nir.tatuani@cloudinary.com>
2022-07-27 11:35:05 +0300

    chore: fixing typos

--- src/test/specs/CloseNewTabSpec.ts
+++ src/test/specs/CloseNewTabSpec.ts
@@ -10,22 +10,22 @@ const OPEN_NEW_TAB: string = "//*[@data-test='open-new-tab']";
  * wdio-allure-ts close tab action
  */
 describeCommon('CloseNewTabSpec', () => {
-  it('open new tab and then close it', async () => {
-    await Reporter.step('open new tab');
+  it('Open new tab and then close it', async () => {
+    await Reporter.step('Open new tab');
     await BrowserUtils.click(OPEN_NEW_TAB);
 
-    await Reporter.step('switch focus to new tab');
+    await Reporter.step('Switch focus to new tab');
     let tabIds: Array<string> = await BrowserUtils.getWindowHandles();
     await BrowserUtils.switchToWindow(tabIds[1]);
 
-    await Reporter.step('close tab');
+    await Reporter.step('Close tab');
     await BrowserUtils.closeWindow();
 
-    await Reporter.step('switch focus current tab');
+    await Reporter.step('Switch the focus for the current tab');
     await BrowserUtils.switchToWindow(tabIds[0]);
 
-    await Reporter.step('validate tab is closed (number of tabs is equal to 1)');
+    await Reporter.step('Validate number of opened tabs is 1');
     tabIds = await BrowserUtils.getWindowHandles();
-    chai.assert.equal(tabIds.length, 1, `expected number of tabs to be 1, but got ${tabIds.length}`);
+    chai.assert.equal(tabIds.length, 1, `Expected number of tabs to be 1, but got ${tabIds.length}`);
   });
 });
