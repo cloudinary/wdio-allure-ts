859f99859f776e731ae1950faf4a1c38840a4059
Felix Zilber <felix@cloudinary.com>
2022-09-07 15:32:40 +0300

    feat: remove chalk

--- src/commons/ConsoleReport.ts
+++ src/commons/ConsoleReport.ts
@@ -1,4 +1,35 @@
-import chalk from 'chalk';
+const colours = {
+  reset: '\x1b[0m',
+  bright: '\x1b[1m',
+  dim: '\x1b[2m',
+  underscore: '\x1b[4m',
+  blink: '\x1b[5m',
+  reverse: '\x1b[7m',
+  hidden: '\x1b[8m',
+
+  fg: {
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+    crimson: '\x1b[38m', // Scarlet
+  },
+  bg: {
+    black: '\x1b[40m',
+    red: '\x1b[41m',
+    green: '\x1b[42m',
+    yellow: '\x1b[43m',
+    blue: '\x1b[44m',
+    magenta: '\x1b[45m',
+    cyan: '\x1b[46m',
+    white: '\x1b[47m',
+    crimson: '\x1b[48m',
+  },
+};
 
 /**
  * Print to standard output
@@ -10,13 +41,13 @@ const printToConsole: boolean = process.env.PRINT_LOGS_TO_CONSOLE === 'true' || 
 let currentTestName: string = '';
 
 const DEBUG: string = '[DEBUG]';
-const DEBUG_COLOR: chalk.Chalk = chalk.gray;
+const DEBUG_COLOR: string = colours.bg.white;
 const STEP: string = '[STEP]';
-const STEP_COLOR: chalk.Chalk = chalk.green;
+const STEP_COLOR: string = colours.bg.green;
 const WARNING: string = '[WARNING]';
-const WARNING_COLOR: chalk.Chalk = chalk.yellow;
+const WARNING_COLOR: string = colours.bg.yellow;
 const ERROR: string = '[ERROR]';
-const ERROR_COLOR: chalk.Chalk = chalk.red;
+const ERROR_COLOR: string = colours.bg.red;
 export namespace ConsoleReport {
   /**
    * Set current executed test name
@@ -71,10 +102,10 @@ export namespace ConsoleReport {
    * @param level message level
    * @param color message color
    */
-  function toConsole(msg: string, level: string, color: chalk.Chalk): void {
+  function toConsole(msg: string, level: string, color: string): void {
     if (printToConsole) {
       const messageToLog: string = prettyMessage(level, msg);
-      console.log(color(messageToLog));
+      console.log(color, messageToLog, colours.reset);
     }
   }
 }
