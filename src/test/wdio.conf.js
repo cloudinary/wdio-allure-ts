ffe6fb471f18e747f4a7a24f793586e987890e13
alexeykagansky <alexey.kagansky@cloudinary.com>
2022-10-25 10:56:23 +0300

    feat: bump wdio

--- src/test/wdio.conf.js
+++ src/test/wdio.conf.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const { Reporter } = require('../index');
 dotenv.config();
 
-const maxChromeInstances = parseInt(process.env.MAX_CHROME_INSTANCES) || 2;
+const maxChromeInstances = parseInt(process.env.MAX_CHROME_INSTANCES) || 10;
 const waitForTimeouts = parseInt(process.env.DEFAULT_TIME_OUT) || 3000;
 const seleniumStandaloneArgs = {
   drivers: {
