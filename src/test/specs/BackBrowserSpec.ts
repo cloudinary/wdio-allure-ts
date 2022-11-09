11528bc295d25c36a7808bd3cd00c6b4dbdb8f9b
FeL <33285268+FelixZilber@users.noreply.github.com>
2022-11-09 15:33:36 +0200

    fix: test-fix-back-button (#316)
    fix: test-fix-back-button

--- src/test/specs/BackBrowserSpec.ts
+++ src/test/specs/BackBrowserSpec.ts
@@ -2,17 +2,15 @@ import { expect } from 'chai';
 import { BrowserUtils, Reporter } from '../..';
 import { describeCommon, sampleAppUrl } from '../TestHelper';
 
-const navigationButton: string = "//button[@data-test='navigate-to-cloudinary']";
-
 describeCommon('BackSpec', () => {
   it('back browser', async () => {
     await Reporter.step('Click navigation button');
-    await BrowserUtils.click(navigationButton);
+    await BrowserUtils.url('https://cloudinary.com/');
 
     await Reporter.step('Click back button');
     await BrowserUtils.back();
 
     await Reporter.step(`Validate url is ${sampleAppUrl}`);
-    expect(await browser.getUrl()).to.equal(sampleAppUrl);
+    expect(await BrowserUtils.getUrl()).to.equal(sampleAppUrl);
   });
 });
