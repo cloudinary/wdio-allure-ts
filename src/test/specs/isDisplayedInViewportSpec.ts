21da9205e25ae7b2f310ffed67633aa50b21a07d
nirtatuani <nir.tatuani@cloudinary.com>
2022-11-15 13:39:30 +0200

    chore: fix elements for isDisplayedInViewportSpec

--- src/test/specs/isDisplayedInViewportSpec.ts
+++ src/test/specs/isDisplayedInViewportSpec.ts
@@ -2,17 +2,17 @@ import { BrowserUtils, Reporter } from '../..';
 import { describeCommon } from '../TestHelper';
 import chai from 'chai';
 
-const existSelector = '#DragAndDropSpec';
-const notExistSelector = '#DragAndDropSpec123';
+const inViewportElement = '#DragAndDropSpec';
+const notInViewportElement = '//*[@data-test="open-new-tab"]';
 
 describeCommon('isDisplayedInViewport', () => {
   it('element is not displayed in viewport', async () => {
     await Reporter.step('check if element is displayed in viewport');
-    chai.expect(await BrowserUtils.isDisplayedInViewport(notExistSelector)).to.be.false;
+    chai.expect(await BrowserUtils.isDisplayedInViewport(inViewportElement)).to.be.false;
   });
 
   it('element displayed in viewport', async () => {
     await Reporter.step('check if element is displayed in viewport');
-    chai.expect(await BrowserUtils.isDisplayedInViewport(existSelector)).to.be.true;
+    chai.expect(await BrowserUtils.isDisplayedInViewport(notInViewportElement)).to.be.true;
   });
 });
