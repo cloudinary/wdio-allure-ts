540b955ec80bc7c1df43714f914cadb543c3980e
nirtatuani <nir.tatuani@cloudinary.com>
2022-12-01 14:44:51 +0200

    chore: fix

--- src/test/specs/isDisplayedInViewportSpec.ts
+++ src/test/specs/isDisplayedInViewportSpec.ts
@@ -8,11 +8,11 @@ const notInViewportElement = '//*[@data-test="open-new-tab"]';
 describeCommon('isDisplayedInViewport', () => {
   it('element is not displayed in viewport', async () => {
     await Reporter.step('check if element is displayed in viewport');
-    chai.expect(await BrowserUtils.isDisplayedInViewport(inViewportElement)).to.be.false;
+    chai.expect(await BrowserUtils.isDisplayedInViewport(notInViewportElement)).to.be.false;
   });
 
   it('element displayed in viewport', async () => {
     await Reporter.step('check if element is displayed in viewport');
-    chai.expect(await BrowserUtils.isDisplayedInViewport(notInViewportElement)).to.be.true;
+    chai.expect(await BrowserUtils.isDisplayedInViewport(inViewportElement)).to.be.true;
   });
 });
